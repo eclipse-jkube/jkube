51d9a43a1d7b6777003cb1528dcb1782988f6784
Rohan Kumar <rohaan@redhat.com>
2019-10-22 19:22:32 +0530

    Relicense project to EPL 2.0 From Apache 2.0

--- jkube-kit/config/image/src/main/java/io/jkube/kit/config/image/build/CleanupMode.java
+++ jkube-kit/config/image/src/main/java/io/jkube/kit/config/image/build/CleanupMode.java
@@ -1,20 +1,17 @@
-package io.jkube.kit.config.image.build;
-/*
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
  *
- * Copyright 2016 Roland Huss
+ *     https://www.eclipse.org/legal/epl-2.0/
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * SPDX-License-Identifier: EPL-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
  */
+package io.jkube.kit.config.image.build;
 
 /**
  * Mode specifying how a cleanup should be performed.
