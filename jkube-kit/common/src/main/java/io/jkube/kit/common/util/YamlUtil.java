6b6d777c4b952b1790ddd8277d9a45a0d9d7e699
Rohan Kumar <rohaan@redhat.com>
2019-11-07 19:30:04 +0530

    + Support integer keys in application.yaml + spring-boot generator can not handle multi-profile configration
    Ported PR https://github.com/fabric8io/fabric8-maven-plugin/pull/1745
    Ported PR https://github.com/fabric8io/fabric8-maven-plugin/pull/1746

--- jkube-kit/common/src/main/java/io/jkube/kit/common/util/YamlUtil.java
+++ jkube-kit/common/src/main/java/io/jkube/kit/common/util/YamlUtil.java
@@ -15,10 +15,15 @@ package io.jkube.kit.common.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.SortedMap;
@@ -26,22 +31,26 @@ import org.yaml.snakeyaml.Yaml;
 
 public class YamlUtil {
     protected static Properties getPropertiesFromYamlResource(URL resource) {
+        return getPropertiesFromYamlResource(null, resource);
+    }
+
+    protected static Properties getPropertiesFromYamlResource(String activeProfile, URL resource) {
         if (resource != null) {
-            try (InputStream yamlStream = resource.openStream()) {
-                Yaml yaml = new Yaml();
-                @SuppressWarnings("unchecked")
-                SortedMap<String, Object> source = yaml.loadAs(yamlStream, SortedMap.class);
+            try {
                 Properties properties = new Properties();
-                if (source != null) {
+                // Splitting file for the possibility of different profiles, by default
+                // only first profile would be considered.
+                List<String> profiles = getYamlListFromFile(resource);
+                if (profiles.size() > 0) {
                     try {
-                        properties.putAll(getFlattenedMap(source));
+                        properties.putAll(getPropertiesFromYamlString(getYamlFromYamlList(activeProfile, profiles)));
                     } catch (IllegalArgumentException e) {
                         throw new IllegalArgumentException(String.format("Spring Boot configuration file %s is not formatted correctly. %s",
                                 resource.toString(), e.getMessage()));
                     }
                 }
                 return properties;
-            } catch (IOException e) {
+            } catch (IOException | URISyntaxException e) {
                 throw new IllegalStateException("Error while reading Yaml resource from URL " + resource, e);
             }
         }
@@ -51,24 +60,26 @@ public class YamlUtil {
     /**
      * Build a flattened representation of the Yaml tree. The conversion is compliant with the thorntail spring-boot rules.
      */
-    private static Map<String, Object> getFlattenedMap(Map<String, Object> source) {
+    private static Map<String, Object> getFlattenedMap(Map<?, ?> source) {
         Map<String, Object> result = new LinkedHashMap<>();
         buildFlattenedMap(result, source, null);
         return result;
     }
 
-    @SuppressWarnings("unchecked")
-    private static void buildFlattenedMap(Map<String, Object> result, Map<String, Object> source, String path) {
-        for (Map.Entry<String, Object> entry : source.entrySet()) {
+    private static void buildFlattenedMap(Map<String, Object> result, Map<?, ?> source, String path) {
+        for (Map.Entry<?, ?> entry : source.entrySet()) {
             Object keyObject = entry.getKey();
 
-            // If user creates a wrong application.yml then we get a runtime classcastexception
-            if (!(keyObject instanceof String)) {
+            String key;
+            if (keyObject instanceof String) {
+                key = (String) keyObject;
+            } else if (keyObject instanceof Number) {
+                key = String.valueOf(keyObject);
+            } else {
+                // If user creates a wrong application.yml then we get a runtime classcastexception
                 throw new IllegalArgumentException(String.format("Expected to find a key of type String but %s with content %s found.",
                         keyObject.getClass(), keyObject.toString()));
             }
-
-            String key = (String) keyObject;
 
             if (path !=null && path.trim().length()>0) {
                 if (key.startsWith("[")) {
@@ -81,11 +92,11 @@ public class YamlUtil {
             Object value = entry.getValue();
             if (value instanceof Map) {
 
-                Map<String, Object> map = (Map<String, Object>) value;
+                Map<?, ?> map = (Map<?, ?>) value;
                 buildFlattenedMap(result, map, key);
             }
             else if (value instanceof Collection) {
-                Collection<Object> collection = (Collection<Object>) value;
+                Collection<?> collection = (Collection<?>) value;
                 int count = 0;
                 for (Object object : collection) {
                     buildFlattenedMap(result,
@@ -96,6 +107,34 @@ public class YamlUtil {
                 result.put(key, (value != null ? value.toString() : ""));
             }
         }
+    }
+
+    public static Properties getPropertiesFromYamlString(String yamlString) throws IllegalArgumentException {
+        Yaml yaml = new Yaml();
+        Properties properties = new Properties();
+
+        @SuppressWarnings("unchecked")
+        SortedMap<String, Object> source = yaml.loadAs(yamlString, SortedMap.class);
+        if (source != null) {
+            properties.putAll(getFlattenedMap(source));
+        }
+        return properties;
+    }
+
+    public static List<String> getYamlListFromFile(URL resource) throws URISyntaxException, IOException {
+        String fileAsString = new String(Files.readAllBytes(Paths.get(resource.toURI())));
+        String[] profiles = fileAsString.split("---");
+        return Arrays.asList(profiles);
+    }
+
+    public static String getYamlFromYamlList(String pattern, List<String> yamlAsStringList) {
+        if (pattern != null) {
+            for (String yamlStr : yamlAsStringList) {
+                if (yamlStr.contains(pattern))
+                    return yamlStr;
+            }
+        }
+        return yamlAsStringList.get(0);
     }
 
 }
