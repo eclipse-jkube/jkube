a8424b227bf3b12e92861718d8798536e495b674
Rohan Kumar <rohaan@redhat.com>
2020-07-21 21:23:32 +0530

    Fix External Resources Maven Quickstart
    + Updated Spring version
    + Update deployment fragments
    + Removed unnecessary dependencies

--- quickstarts/maven/external-resources/src/main/java/org/eclipse/jkube/maven/sample/external/resources/Application.java
+++ quickstarts/maven/external-resources/src/main/java/org/eclipse/jkube/maven/sample/external/resources/Application.java
@@ -15,14 +15,8 @@ package org.eclipse.jkube.maven.sample.external.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-@EnableCircuitBreaker
-@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
