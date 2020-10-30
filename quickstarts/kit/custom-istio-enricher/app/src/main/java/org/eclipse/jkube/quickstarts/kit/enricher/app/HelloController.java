f00d488b0c3ca073797716594d4312f867606035
Rohan Kumar <rohaan@redhat.com>
2020-10-30 21:42:18 +0530

    Added a Quickstart for Implementing your own Custom Enricher
    Added a Quickstart for JKube Kit Enricher API on how you can extend
    JKube Kit and add an enricher for your own Custom Use case. This
    quickstart contains a simple Spring Boot application which uses a custom
    made enricher to generate dummy Istio Gateway.

---
+++ quickstarts/kit/custom-istio-enricher/app/src/main/java/org/eclipse/jkube/quickstarts/kit/enricher/app/HelloController.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.quickstarts.kit.enricher.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
