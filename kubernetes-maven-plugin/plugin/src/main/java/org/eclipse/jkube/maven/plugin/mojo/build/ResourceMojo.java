d12f920310e0ba5dbaaab4ebe436c9c0a4e391b7
rnetuka <rnetuka@redhat.com>
2021-03-30 17:32:01 +0200

    Re-introduce jkube.replicas property
    Signed-off-by: rnetuka <rnetuka@redhat.com>

--- kubernetes-maven-plugin/plugin/src/main/java/org/eclipse/jkube/maven/plugin/mojo/build/ResourceMojo.java
+++ kubernetes-maven-plugin/plugin/src/main/java/org/eclipse/jkube/maven/plugin/mojo/build/ResourceMojo.java
@@ -196,6 +196,9 @@ public class ResourceMojo extends AbstractJKubeMojo {
     @Parameter(property="jkube.interpolateTemplateParameters", defaultValue = "true")
     private Boolean interpolateTemplateParameters;
 
+    @Parameter(property = "jkube.replicas")
+    private Integer replicas;
+
     @Component
     private MavenProjectHelper projectHelper;
 
@@ -251,6 +254,9 @@ public class ResourceMojo extends AbstractJKubeMojo {
         realResourceDir = ResourceUtil.getFinalResourceDir(resourceDir, environment);
         if (namespace != null && !namespace.isEmpty()) {
             resources = ResourceConfig.toBuilder(resources).namespace(namespace).build();
+        }
+        if (replicas != null) {
+            resources = ResourceConfig.toBuilder(resources).replicas(replicas).build();
         }
         final ResourceServiceConfig resourceServiceConfig = ResourceServiceConfig.builder()
             .project(javaProject)
