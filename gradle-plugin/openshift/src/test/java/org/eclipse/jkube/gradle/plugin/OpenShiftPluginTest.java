e2618b85c553cf3626b3da77ce5189b264a49879
Marc Nuri <marc@marcnuri.com>
2021-07-15 16:47:06 +0200

    feat: Basic Gradle Plugin implementation (#764)
    Project structure, dependencies, etc.
    
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

---
+++ gradle-plugin/openshift/src/test/java/org/eclipse/jkube/gradle/plugin/OpenShiftPluginTest.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import org.eclipse.jkube.gradle.plugin.task.KubernetesApplyTask;
+import org.eclipse.jkube.gradle.plugin.task.KubernetesBuildTask;
+import org.eclipse.jkube.gradle.plugin.task.KubernetesResourceTask;
+
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class OpenShiftPluginTest {
+
+  private Project project;
+
+  @Before
+  public void setUp() {
+    project = mock(Project.class, RETURNS_DEEP_STUBS);
+  }
+
+  @Test
+  public void apply_withValidProject_shouldCreateExtensionAndRegisterTasks() {
+    // When
+    new OpenShiftPlugin().apply(project);
+    // Then
+    verify(project.getExtensions(), times(1))
+        .create("openshift", OpenShiftExtension.class);
+    verify(project.getTasks(), times(1))
+        .register("ocBuild", KubernetesBuildTask.class, OpenShiftExtension.class);
+    verify(project.getTasks(), times(1))
+        .register("ocResource", KubernetesResourceTask.class, OpenShiftExtension.class);
+    verify(project.getTasks(), times(1))
+        .register("ocApply", KubernetesApplyTask.class, OpenShiftExtension.class);
+  }
+
+  @Test
+  public void configurePrecedence_withValidProject_shouldReturnTaskPrecedence() {
+    //When
+    final Map<String, Collection<Class<? extends Task>>> result = new OpenShiftPlugin().getTaskPrecedence();
+    // Then
+    assertThat(result)
+        .hasSize(1)
+        .containsEntry("ocApply", Collections.singletonList(KubernetesResourceTask.class));
+  }
+}
