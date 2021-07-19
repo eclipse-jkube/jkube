ef9c2239bf90671c89e7c3832d329ba2831f55bb
Marc Nuri <marc@marcnuri.com>
2021-07-19 14:32:01 +0200

    test: Bootstrapped Integration Tests module (#764)
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

---
+++ gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/SimpleIT.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.tests;
+
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleIT {
+
+  @Test
+  public void tasks_containsKubernetesAndOpenShiftTasks() throws Exception {
+    final String baseDir = System.getProperty("itDir", "");
+    final BuildResult result = GradleRunner.create()
+        .withGradleDistribution(new URI("https://services.gradle.org/distributions/gradle-6.8-bin.zip"))
+        .withDebug(true)
+        .withProjectDir(new File(baseDir).toPath().resolve("src").resolve("it").resolve("simple").toFile())
+        .withPluginClasspath(Arrays.asList(
+            module(baseDir, "jkube-kit", "common"),
+            module(baseDir, "jkube-kit", "config", "resource"),
+            module(baseDir, "jkube-kit", "config", "service"),
+            module(baseDir, "gradle-plugin", "kubernetes"),
+            module(baseDir, "gradle-plugin", "openshift")
+        ))
+        .withArguments("tasks")
+        .build();
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("Kubernetes tasks")
+        .contains("k8sApply - ")
+        .contains("k8sBuild - ")
+        .contains("k8sResource - ")
+        .contains("Openshift tasks")
+        .contains("ocApply - ")
+        .contains("ocBuild - ")
+        .contains("ocResource - ");
+  }
+
+  private static File module(String baseDir, String... subdirs) {
+    Path dir = new File(baseDir).getAbsoluteFile().toPath().getParent().getParent();
+    for (String subdir : subdirs) {
+      dir = dir.resolve(subdir);
+    }
+    return dir.resolve("target").resolve("classes").toFile();
+  }
+}
