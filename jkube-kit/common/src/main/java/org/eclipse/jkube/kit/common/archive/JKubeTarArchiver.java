f71afbbf2b5e71ae954f23a8396b8e91545df6b2
Rohan Kumar <rohaan@redhat.com>
2021-07-27 17:47:14 +0530

    Fix #777: Failure in processing generated tarball by old Docker versions
    Tar files generated during `k8s:build` were getting rejected from Docker
    v1.12.1 (API version 1.24). Not setting size of TarArchiveEntry in case
    of directories seem to fix this issue
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- jkube-kit/common/src/main/java/org/eclipse/jkube/kit/common/archive/JKubeTarArchiver.java
+++ jkube-kit/common/src/main/java/org/eclipse/jkube/kit/common/archive/JKubeTarArchiver.java
@@ -74,13 +74,7 @@ public class JKubeTarArchiver {
         String relativeFilePath = inputDirectory.toURI().relativize(
             new File(currentFile.getAbsolutePath()).toURI()).getPath();
 
-        final TarArchiveEntry tarEntry = new TarArchiveEntry(currentFile, relativeFilePath);
-        tarEntry.setSize(currentFile.length());
-        if (fileModeMap.containsKey(currentFile)) {
-          tarEntry.setMode(Integer.parseInt(fileModeMap.get(currentFile), 8));
-        } else if (currentFile.isDirectory()) {
-          tarEntry.setMode(TarArchiveEntry.DEFAULT_DIR_MODE);
-        }
+        final TarArchiveEntry tarEntry = createTarArchiveEntry(fileModeMap, currentFile, relativeFilePath);
         Optional.ofNullable(tarArchiveEntryCustomizer).ifPresent(tac -> tac.accept(tarEntry));
         tarArchiveOutputStream.putArchiveEntry(tarEntry);
         if (currentFile.isFile()) {
@@ -92,5 +86,17 @@ public class JKubeTarArchiver {
     }
 
     return outputFile;
+  }
+
+  static TarArchiveEntry createTarArchiveEntry(Map<File, String> fileModeMap, File currentFile, String relativeFilePath) {
+    final TarArchiveEntry tarEntry = new TarArchiveEntry(currentFile, relativeFilePath);
+    tarEntry.setSize(currentFile.length());
+    if (fileModeMap.containsKey(currentFile)) {
+      tarEntry.setMode(Integer.parseInt(fileModeMap.get(currentFile), 8));
+    } else if (currentFile.isDirectory()) {
+      tarEntry.setSize(0L);
+      tarEntry.setMode(TarArchiveEntry.DEFAULT_DIR_MODE);
+    }
+    return tarEntry;
   }
 }
