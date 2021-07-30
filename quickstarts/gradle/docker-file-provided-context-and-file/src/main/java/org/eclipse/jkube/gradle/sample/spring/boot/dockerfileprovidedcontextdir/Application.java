9b772534330a20d8beac083f368833c4a0095efa
Rohan Kumar <rohaan@redhat.com>
2021-07-30 21:51:12 +0530

    Dockerfile Provided Gradle Quickstart
    Add quickstart for Dockerfile with context and assembly
    support. Added four different quickstarts for four different
    profiles.
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ quickstarts/gradle/docker-file-provided-context-and-file/src/main/java/org/eclipse/jkube/gradle/sample/spring/boot/dockerfileprovidedcontextdir/Application.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.sample.spring.boot.dockerfileprovidedcontextdir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot with provided Dockerfile!";
+  }
+}
