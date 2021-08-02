488ed1fa976afb12c1e1e563b2923f52a9a0207d
Vladislav Laetansky <laetansky@gmail.com>
2021-08-02 11:53:10 +0300

    TemplateUtilTest use Parameterized test with AssertJ
    Signed-off-by: Vladislav Laetansky <vlaetansky@gmail.com>

--- jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/TemplateUtilTest.java
+++ jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/TemplateUtilTest.java
@@ -3,29 +3,50 @@
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at:
- *
- *     https://www.eclipse.org/legal/epl-2.0/
- *
+ * <p>
+ * https://www.eclipse.org/legal/epl-2.0/
+ * <p>
  * SPDX-License-Identifier: EPL-2.0
- *
+ * <p>
  * Contributors:
- *   Red Hat, Inc. - initial API and implementation
+ * Red Hat, Inc. - initial API and implementation
  */
 package org.eclipse.jkube.kit.common.util;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.jkube.kit.common.util.TemplateUtil.escapeYamlTemplate;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Parameterized.class)
 public class TemplateUtilTest {
 
-  @Test
-  public void escapeYamlTemplateTest() {
-    assertThat(escapeYamlTemplate("abcd"), is("abcd"));
-    assertThat(escapeYamlTemplate("abc{de}f}"), is("abc{de}f}"));
-    assertThat(escapeYamlTemplate("abc{{de}f"), is("abc{{\"{{\"}}de}f"));
-    assertThat(escapeYamlTemplate("abc{{de}f}}"), is("abc{{\"{{\"}}de}f{{\"}}\"}}"));
-  }
+    @Parameterized.Parameters(name = "{0} is {1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"abcd", "abcd"},
+                {"abc{de}f}", "abc{de}f}"},
+                {"abc{{de}f", "abc{{\"{{\"}}de}f"},
+                {"abc{{de}f}}", "abc{{\"{{\"}}de}f{{\"}}\"}}"}
+        });
+    }
+
+    private final String input;
+
+    private final String expected;
+
+    public TemplateUtilTest(String input, String expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Test
+    public void escapeYamlTemplateTest() {
+        assertThat(escapeYamlTemplate(input)).isEqualTo(expected);
+    }
 }
