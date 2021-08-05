bbaccebf0ca6e38c6091adbc74d0d77740fb807d
Marc Nuri <marc@marcnuri.com>
2021-08-05 12:47:08 +0200

    test: Kubernetes Extension configuration IT
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

---
+++ gradle-plugin/kubernetes/src/test/java/org/eclipse/jkube/gradle/plugin/task/KubernetesConfigViewTaskTest.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.task;
+
+import org.eclipse.jkube.gradle.plugin.KubernetesExtension;
+import org.eclipse.jkube.gradle.plugin.TestKubernetesExtension;
+import org.eclipse.jkube.kit.config.image.build.JKubeBuildStrategy;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.MockedConstruction;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class KubernetesConfigViewTaskTest {
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  private MockedConstruction<DefaultTask> defaultTaskMockedConstruction;
+  private Project project;
+  private Logger logger;
+  private KubernetesExtension extension;
+
+  @Before
+  public void setUp() throws IOException {
+    project = mock(Project.class, RETURNS_DEEP_STUBS);
+    logger = mock(Logger.class);
+    extension = new TestKubernetesExtension();
+    defaultTaskMockedConstruction = mockConstruction(DefaultTask.class, (mock, ctx) -> {
+      when(mock.getProject()).thenReturn(project);
+      when(mock.getLogger()).thenReturn(logger);
+    });
+    when(project.getProjectDir()).thenReturn(temporaryFolder.getRoot());
+    when(project.getBuildDir()).thenReturn(temporaryFolder.newFolder("build"));
+    when(project.getConfigurations().stream()).thenAnswer(i -> Stream.empty());
+    when(project.getBuildscript().getConfigurations().stream()).thenAnswer(i -> Stream.empty());
+    when(project.getProperties()).thenReturn(Collections.emptyMap());
+    when(project.getExtensions().getByType(KubernetesExtension.class)).thenReturn(extension);
+  }
+
+  @After
+  public void tearDown() {
+    defaultTaskMockedConstruction.close();
+  }
+
+  @Test
+  public void runTask_withImplementationPending_shouldThrowException() {
+    // Given
+    extension.buildStrategy = JKubeBuildStrategy.s2i;
+    extension.getOffline().set(true);
+    final KubernetesConfigViewTask configViewTask = new KubernetesConfigViewTask(KubernetesExtension.class);
+    // When
+    configViewTask.runTask();
+    // Then
+    verify(logger, times(1))
+        .lifecycle(matches("k8s: \n---\noffline: true\nbuildStrategy: \"s2i\""));
+  }
+}
