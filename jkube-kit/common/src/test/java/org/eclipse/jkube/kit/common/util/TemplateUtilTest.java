5a1828c0ece30567fdcf89ce7668bb8c2003211d
Vladislav Laetansky <vlaetansky@gmail.com>
2021-08-11 17:41:27 +0300

    License restore formatting
    Signed-off-by: Vladislav Laetansky <vlaetansky@gmail.com>

--- jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/TemplateUtilTest.java
+++ jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/TemplateUtilTest.java
@@ -3,13 +3,13 @@
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at:
- * <p>
- * https://www.eclipse.org/legal/epl-2.0/
- * <p>
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
  * SPDX-License-Identifier: EPL-2.0
- * <p>
+ *
  * Contributors:
- * Red Hat, Inc. - initial API and implementation
+ *   Red Hat, Inc. - initial API and implementation
  */
 package org.eclipse.jkube.kit.common.util;
 
