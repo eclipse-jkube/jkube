71289cd631a65f16b004165b3644a00ed1f0b9e5
Rohan Kumar <rohaan@redhat.com>
2021-08-16 15:59:10 +0530

    Add support for KubernetesResourceTask in gradle plugin
    + Port resource goal functionality from ResourceMojo to KubernetesResourceTask
    + Add groovy configuration for resource/enricher configuration in
      groovy-dsl-config quickstart and micronaut-customized-image quickstart
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ gradle-plugin/kubernetes/src/test/java/org/eclipse/jkube/gradle/plugin/task/KubernetesResourceTaskTest.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.task;
+
+import org.eclipse.jkube.gradle.plugin.KubernetesExtension;
+import org.eclipse.jkube.gradle.plugin.TestKubernetesExtension;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.MockedConstruction;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
+
+public class KubernetesResourceTaskTest {
+  private MockedConstruction<DefaultTask> defaultTaskMockedConstruction;
+  private Project project;
+  private KubernetesExtension extension;
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  @Before
+  public void setUp() throws IOException {
+    project = mock(Project.class, RETURNS_DEEP_STUBS);
+    defaultTaskMockedConstruction = mockConstruction(DefaultTask.class, (mock, ctx) -> {
+      when(mock.getProject()).thenReturn(project);
+      when(mock.getLogger()).thenReturn(mock(Logger.class));
+    });
+    extension = new TestKubernetesExtension();
+    when(project.getExtensions().getByType(KubernetesExtension.class)).thenReturn(extension);
+    when(project.getConfigurations().stream()).thenAnswer(i -> Stream.empty());
+    when(project.getBuildscript().getConfigurations().stream()).thenAnswer(i -> Stream.empty());
+    when(project.getProjectDir()).thenReturn(temporaryFolder.getRoot());
+    when(project.getBuildDir()).thenReturn(temporaryFolder.newFolder("build"));
+  }
+
+  @After
+  public void tearDown() {
+    defaultTaskMockedConstruction.close();
+  }
+
+  @Test
+  public void runTask_withImageConfiguration_shouldGenerateResources() {
+    // Given
+    KubernetesResourceTask resourceTask = new KubernetesResourceTask(KubernetesExtension.class);
+
+    // When
+    resourceTask.runTask();
+
+    // Then
+    assertThat(new File(temporaryFolder.getRoot(), "build/META-INF/jkube/kubernetes.yml")).exists();
+  }
+}
