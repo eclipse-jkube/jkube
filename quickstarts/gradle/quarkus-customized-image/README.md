17b0d6553fa3e60442228cabaa1005f96ab4ce2a
Rohan Kumar <rohaan@redhat.com>
2021-08-20 17:34:54 +0530

    Port Quarkus Customized Image maven quickstart to gradle
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ quickstarts/gradle/quarkus-customized-image/README.md
@@ -0,0 +1,24 @@
+# Eclipse JKube Quarkus with Customized Image Quickstart
+
+A simple REST application demonstrating usage of Eclipse JKube with Quarkus
+customized to use an official
+[Red Hat Container Image](https://catalog.redhat.com/software/containers/search).
+
+## Requirements:
+
+- JDK 8 or 11+
+- OpenShift Cluster (OpenShift, CRC, etc.)
+- Registered pull secret in your cluster ([Registry Service Accounts](https://access.redhat.com/terms-based-registry/#/accounts))
+
+
+## How to run
+
+**Note:**
+> To be able to retrieve the image from the catalog you'll have to modify the
+configuration field `openshiftPullSecret = '12819530-ocp42-exposed-env-pull-secret-pull-secret'`
+`openshift` section in your `build.gradle`.
+
+```shell script
+$ gradle ocBuild
+```
+
---
+++ quickstarts/gradle/quarkus-customized-image/src/main/java/org/eclipse/jkube/quickstarts/gradle/CoolApplicationResource.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.quickstarts.gradle;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class CoolApplicationResource {
+
+    private CoolApplicationService coolApplicationService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get() {
+        return Response.ok(coolApplicationService.getCoolApplication()).build();
+    }
+
+    @Inject
+    public void setCoolApplicationService(CoolApplicationService coolApplicationService) {
+        this.coolApplicationService = coolApplicationService;
+    }
+}
