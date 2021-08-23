5dcc688c36742a04bfb009e3e6325d8c55de067e
Rohan Kumar <rohaan@redhat.com>
2021-08-23 15:22:05 +0530

    Fix #851: Add KubernetesLogTask to Kubernetes Gradle Plugin
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- gradle-plugin/openshift/src/main/java/org/eclipse/jkube/gradle/plugin/OpenShiftExtension.java
+++ gradle-plugin/openshift/src/main/java/org/eclipse/jkube/gradle/plugin/OpenShiftExtension.java
@@ -13,10 +13,20 @@
  */
 package org.eclipse.jkube.gradle.plugin;
 
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.eclipse.jkube.kit.common.JavaProject;
+import org.eclipse.jkube.kit.common.KitLogger;
+import org.eclipse.jkube.kit.common.util.OpenshiftHelper;
 import org.eclipse.jkube.kit.config.resource.PlatformMode;
 import org.eclipse.jkube.kit.config.resource.RuntimeMode;
+import org.gradle.api.provider.Property;
+
+import java.io.File;
 
 public abstract class OpenShiftExtension extends KubernetesExtension {
+  public static final String DEFAULT_OPENSHIFT_MANIFEST = "build/META-INF/jkube/openshift.yml";
+
+  public abstract Property<File> getOpenShiftManifest();
 
   @Override
   public RuntimeMode getRuntimeMode() {
@@ -26,5 +36,13 @@ public abstract class OpenShiftExtension extends KubernetesExtension {
   @Override
   public PlatformMode getPlatformMode() {
     return PlatformMode.openshift;
+  }
+
+  @Override
+  public File getManifest(KitLogger kitLogger, KubernetesClient kubernetesClient, JavaProject javaProject) {
+    if (OpenshiftHelper.isOpenShift(kubernetesClient)) {
+      return getOpenShiftManifest().getOrElse(new File(javaProject.getBaseDirectory(), DEFAULT_OPENSHIFT_MANIFEST));
+    }
+    return getKubernetesManifestOrDefault(javaProject);
   }
 }
