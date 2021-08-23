83f17f9dfbeba60d43cfe48625547f49cf0aa748
Rohan Kumar <rohaan@redhat.com>
2021-08-23 15:22:05 +0530

    Fix #851: Add KubernetesLogTask to Kubernetes Gradle Plugin
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- gradle-plugin/kubernetes/src/main/java/org/eclipse/jkube/gradle/plugin/KubernetesPlugin.java
+++ gradle-plugin/kubernetes/src/main/java/org/eclipse/jkube/gradle/plugin/KubernetesPlugin.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import org.eclipse.jkube.gradle.plugin.task.KubernetesApplyTask;
 import org.eclipse.jkube.gradle.plugin.task.KubernetesBuildTask;
 import org.eclipse.jkube.gradle.plugin.task.KubernetesConfigViewTask;
+import org.eclipse.jkube.gradle.plugin.task.KubernetesLogTask;
 import org.eclipse.jkube.gradle.plugin.task.KubernetesResourceTask;
 
 import org.gradle.api.Project;
@@ -45,6 +46,7 @@ public class KubernetesPlugin extends AbstractJKubePlugin<KubernetesExtension> {
     register(project, "k8sBuild", KubernetesBuildTask.class);
     register(project, "k8sResource", KubernetesResourceTask.class);
     register(project, "k8sApply", KubernetesApplyTask.class);
+    register(project, "k8sLog", KubernetesLogTask.class);
   }
 
 }
