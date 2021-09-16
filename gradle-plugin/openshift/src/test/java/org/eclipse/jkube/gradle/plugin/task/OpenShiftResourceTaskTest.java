c652fbab7490fea85e860e0eb5ac6eaf120b51ba
Rohan Kumar <rohaan@redhat.com>
2021-09-16 22:29:52 +0530

    Use DOCKER_IMAGE_USER property from ImageNameFormatter in OpenShiftResourceTask
    This property was wrongly named in ResourceMojo, for maven plugins
    this got fixed in #910 and later refactored to use static constant
    from ImageNameFormatter.
    
    Use same static constant in OpenShiftResourceTask
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- gradle-plugin/openshift/src/test/java/org/eclipse/jkube/gradle/plugin/task/OpenShiftResourceTaskTest.java
+++ gradle-plugin/openshift/src/test/java/org/eclipse/jkube/gradle/plugin/task/OpenShiftResourceTaskTest.java
@@ -13,8 +13,12 @@
  */
 package org.eclipse.jkube.gradle.plugin.task;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
 import org.eclipse.jkube.gradle.plugin.OpenShiftExtension;
 import org.eclipse.jkube.gradle.plugin.TestOpenShiftExtension;
+import org.eclipse.jkube.kit.common.util.KubernetesHelper;
+import org.eclipse.jkube.kit.config.image.ImageConfiguration;
+import org.eclipse.jkube.kit.config.image.build.BuildConfiguration;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
@@ -25,12 +29,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
@@ -43,6 +51,7 @@ public class OpenShiftResourceTaskTest {
 
   private MockedConstruction<DefaultTask> defaultTaskMockedConstruction;
   private Project project;
+  private TestOpenShiftExtension testOpenShiftExtension;
 
   @Before
   public void setUp() throws IOException {
@@ -51,11 +60,14 @@ public class OpenShiftResourceTaskTest {
       when(mock.getProject()).thenReturn(project);
       when(mock.getLogger()).thenReturn(mock(Logger.class));
     });
+    testOpenShiftExtension = new TestOpenShiftExtension();
+    when(project.getGroup()).thenReturn("org.eclipse.jkube.testing");
+    when(project.getName()).thenReturn("test-project");
     when(project.getProjectDir()).thenReturn(temporaryFolder.getRoot());
     when(project.getBuildDir()).thenReturn(temporaryFolder.newFolder("build"));
     when(project.getConfigurations().stream()).thenAnswer(i -> Stream.empty());
     when(project.getBuildscript().getConfigurations().stream()).thenAnswer(i -> Stream.empty());
-    when(project.getExtensions().getByType(OpenShiftExtension.class)).thenReturn(new TestOpenShiftExtension());
+    when(project.getExtensions().getByType(OpenShiftExtension.class)).thenReturn(testOpenShiftExtension);
     when(project.getConvention().getPlugin(JavaPluginConvention.class)).thenReturn(mock(JavaPluginConvention.class));
   }
 
@@ -77,5 +89,32 @@ public class OpenShiftResourceTaskTest {
       .hasContent("---\n" +
         "apiVersion: v1\n" +
         "kind: List\n");
+  }
+
+  @Test
+  public void runTask_resolvesGroupInImageNameToNamespaceSetViaConfiguration_whenNoNamespaceDetected() {
+    try (MockedStatic<KubernetesHelper> kubernetesHelper = Mockito.mockStatic(KubernetesHelper.class)) {
+      // Given
+      kubernetesHelper.when(KubernetesHelper::getDefaultNamespace).thenReturn("test-custom-namespace");
+      kubernetesHelper.when(() -> KubernetesHelper.getKind(any())).thenReturn("DeploymentConfig");
+      kubernetesHelper.when(() -> KubernetesHelper.getName((HasMetadata) any())).thenReturn("test-project");
+      ImageConfiguration imageConfiguration = ImageConfiguration.builder()
+        .name("%g/%a")
+        .build(BuildConfiguration.builder()
+          .from("test-base-image:latest")
+          .build())
+        .build();
+      testOpenShiftExtension.images = Collections.singletonList(imageConfiguration);
+      OpenShiftResourceTask resourceTask = new OpenShiftResourceTask(OpenShiftExtension.class);
+
+      // When
+      resourceTask.runTask();
+
+      // Then
+      assertThat(resourceTask.resolvedImages)
+        .hasSize(1)
+        .singleElement()
+        .hasFieldOrPropertyWithValue("name", "test-custom-namespace/test-project");
+    }
   }
 }
