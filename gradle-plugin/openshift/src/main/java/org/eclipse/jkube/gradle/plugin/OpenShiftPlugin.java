0490d309bab5273d829fcbe7246fd9a90daf7aea
Rohan Kumar <rohaan@redhat.com>
2021-09-21 12:36:50 +0530

    Fix #921: Implement `oc:apply` equivalent functionality for OpenShift Gradle Plugin
    Add OpenShiftApplyTask to OpenShift Gradle Plugin which just extends
    KubernetesApplyTask. There was no need to override any behavior from
    KubernetesApplyTask since all configuration options are overridden in
    case of OpenShiftExtension.
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- gradle-plugin/openshift/src/main/java/org/eclipse/jkube/gradle/plugin/OpenShiftPlugin.java
+++ gradle-plugin/openshift/src/main/java/org/eclipse/jkube/gradle/plugin/OpenShiftPlugin.java
@@ -18,14 +18,14 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.jkube.gradle.plugin.task.KubernetesApplyTask;
 import org.eclipse.jkube.gradle.plugin.task.KubernetesConfigViewTask;
 import org.eclipse.jkube.gradle.plugin.task.KubernetesLogTask;
 import org.eclipse.jkube.gradle.plugin.task.KubernetesResourceTask;
-
 import org.eclipse.jkube.gradle.plugin.task.KubernetesUndeployTask;
+import org.eclipse.jkube.gradle.plugin.task.OpenShiftApplyTask;
 import org.eclipse.jkube.gradle.plugin.task.OpenShiftBuildTask;
 import org.eclipse.jkube.gradle.plugin.task.OpenShiftResourceTask;
+
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 
@@ -47,7 +47,7 @@ public class OpenShiftPlugin extends AbstractJKubePlugin<OpenShiftExtension> {
     register(project, "ocConfigView", KubernetesConfigViewTask.class);
     register(project, "ocBuild", OpenShiftBuildTask.class);
     register(project, "ocResource", OpenShiftResourceTask.class);
-    register(project, "ocApply", KubernetesApplyTask.class);
+    register(project, "ocApply", OpenShiftApplyTask.class);
     register(project, "ocLog", KubernetesLogTask.class);
     register(project, "ocUndeploy", KubernetesUndeployTask.class);
   }
