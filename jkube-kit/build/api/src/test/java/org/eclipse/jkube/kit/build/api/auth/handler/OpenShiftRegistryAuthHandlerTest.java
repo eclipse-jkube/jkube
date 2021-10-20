3594e883ccafc87f2c9dee9bb24417961ed1db33
dolatapatryk <dolata.patryk1@gmail.com>
2021-10-20 07:40:08 +0200

    Get rid of wildcard import
    Signed-off-by: dolatapatryk <dolata.patryk1@gmail.com>

--- jkube-kit/build/api/src/test/java/org/eclipse/jkube/kit/build/api/auth/handler/OpenShiftRegistryAuthHandlerTest.java
+++ jkube-kit/build/api/src/test/java/org/eclipse/jkube/kit/build/api/auth/handler/OpenShiftRegistryAuthHandlerTest.java
@@ -33,7 +33,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * @author roland
