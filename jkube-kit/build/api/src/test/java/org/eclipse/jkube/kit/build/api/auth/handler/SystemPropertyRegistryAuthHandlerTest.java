e92429c2b823d0db416797c97beb3d7d5d8d659e
Patryk <dolata.patryk1@gmail.com>
2021-10-25 08:07:22 +0200

    Remove deprecated ExpectedException and Rule annotation, use assertThrows instead in test classes (#1042)
    * Remove deprecated ExpectedException and Rule annotation, use assertThrows instead in test classes.
    
    Signed-off-by: dolatapatryk <dolata.patryk1@gmail.com>
    
    * Get rid of wildcard import
    
    Signed-off-by: dolatapatryk <dolata.patryk1@gmail.com>
    
    * Revert changes for KeyStoreUtilsTest
    
    Signed-off-by: dolatapatryk <dolata.patryk1@gmail.com>
    
    * Get rid of wildcard import in DockerRegistryAuthHandlerTest and update changelog
    
    Signed-off-by: dolatapatryk <dolata.patryk1@gmail.com>

--- jkube-kit/build/api/src/test/java/org/eclipse/jkube/kit/build/api/auth/handler/SystemPropertyRegistryAuthHandlerTest.java
+++ jkube-kit/build/api/src/test/java/org/eclipse/jkube/kit/build/api/auth/handler/SystemPropertyRegistryAuthHandlerTest.java
@@ -18,94 +18,89 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Base64;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import org.eclipse.jkube.kit.build.api.auth.AuthConfig;
 import org.eclipse.jkube.kit.build.api.auth.RegistryAuthConfig;
 import org.eclipse.jkube.kit.common.KitLogger;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import mockit.Mocked;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 
 /**
  * @author roland
  */
 public class SystemPropertyRegistryAuthHandlerTest {
 
-    @Mocked
-    KitLogger log;
+  @Mocked
+  KitLogger log;
 
-    SystemPropertyRegistryAuthHandler handler;
+  SystemPropertyRegistryAuthHandler handler;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+  @Before
+  public void setup() {
+    RegistryAuthConfig registryAuthConfig = RegistryAuthConfig.builder()
+        .skipExtendedAuthentication(false)
+        .propertyPrefix("jkube.docker")
+        .build();
+    handler = new SystemPropertyRegistryAuthHandler(registryAuthConfig, log);
+  }
 
-    @Before
-    public void setup() {
-        RegistryAuthConfig registryAuthConfig = RegistryAuthConfig.builder()
-          .skipExtendedAuthentication(false)
-          .propertyPrefix("jkube.docker")
-          .build();
-        handler = new SystemPropertyRegistryAuthHandler(registryAuthConfig, log);
+  @Test
+  public void testEmpty() throws Exception {
+    String userHome = System.getProperty("user.home");
+    try {
+      File tempDir = Files.createTempDirectory("d-m-p").toFile();
+      System.setProperty("user.home", tempDir.getAbsolutePath());
+      assertThat(handler.create(RegistryAuthConfig.Kind.PUSH, null, "blubberbla:1611", s -> s)).isNull();
+    } finally {
+      System.setProperty("user.home", userHome);
     }
+  }
 
-
-    @Test
-    public void testEmpty() throws Exception {
-        String userHome = System.getProperty("user.home");
-        try {
-            File tempDir = Files.createTempDirectory("d-m-p").toFile();
-            System.setProperty("user.home", tempDir.getAbsolutePath());
-            assertThat(handler.create(RegistryAuthConfig.Kind.PUSH, null, "blubberbla:1611", s->s)).isNull();
-        } finally {
-            System.setProperty("user.home", userHome);
-        }
+  @Test
+  public void testSystemProperty() throws Exception {
+    System.setProperty("jkube.docker.push.username", "roland");
+    System.setProperty("jkube.docker.push.password", "secret");
+    System.setProperty("jkube.docker.push.email", "roland@jolokia.org");
+    try {
+      AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, null, null, s -> s);
+      verifyAuthConfig(config, "roland", "secret", "roland@jolokia.org");
+    } finally {
+      System.clearProperty("jkube.docker.push.username");
+      System.clearProperty("jkube.docker.push.password");
+      System.clearProperty("jkube.docker.push.email");
     }
+  }
 
+  @Test
+  public void testSystemPropertyNoPassword() throws IOException {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> checkException("jkube.docker.username"));
+    assertThat(exception).hasMessageContaining("No password provided for username secret");
+  }
 
-    @Test
-    public void testSystemProperty() throws Exception {
-        System.setProperty("jkube.docker.push.username", "roland");
-        System.setProperty("jkube.docker.push.password", "secret");
-        System.setProperty("jkube.docker.push.email", "roland@jolokia.org");
-        try {
-            AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, null, null, s -> s);
-            verifyAuthConfig(config,"roland","secret","roland@jolokia.org");
-        } finally {
-            System.clearProperty("jkube.docker.push.username");
-            System.clearProperty("jkube.docker.push.password");
-            System.clearProperty("jkube.docker.push.email");
-        }
+  private void checkException(String key) throws IOException {
+    System.setProperty(key, "secret");
+    try {
+      handler.create(RegistryAuthConfig.Kind.PUSH, null, null, s -> s);
+    } finally {
+      System.clearProperty(key);
     }
+  }
 
-
-    @Test
-    public void testSystemPropertyNoPassword() throws IOException {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("No password provided for username secret");
-        checkException("jkube.docker.username");
+  private void verifyAuthConfig(AuthConfig config, String username, String password, String email) {
+    JsonObject params = new Gson().fromJson(new String(Base64.getDecoder().decode(config.toHeaderValue(log).getBytes())),
+        JsonObject.class);
+    assertThat(params.get("username").getAsString()).isEqualTo(username);
+    assertThat(params.get("password").getAsString()).isEqualTo(password);
+    if (email != null) {
+      assertThat(params.get("email").getAsString()).isEqualTo(email);
     }
-
-    private void checkException(String key) throws IOException {
-        System.setProperty(key, "secret");
-        try {
-            handler.create(RegistryAuthConfig.Kind.PUSH, null, null, s -> s);
-        } finally {
-            System.clearProperty(key);
-        }
-    }
-
-    private void verifyAuthConfig(AuthConfig config, String username, String password, String email) {
-        JsonObject params = new Gson().fromJson(new String(Base64.getDecoder().decode(config.toHeaderValue(log).getBytes())), JsonObject.class);
-        assertThat(params.get("username").getAsString()).isEqualTo(username);
-        assertThat(params.get("password").getAsString()).isEqualTo(password);
-        if (email != null) {
-            assertThat(params.get("email").getAsString()).isEqualTo(email);
-        }
-    }
+  }
 
 }
--- jkube-kit/build/service/docker/src/test/java/org/eclipse/jkube/kit/build/service/docker/auth/ecr/DockerRegistryAuthHandlerTest.java
+++ jkube-kit/build/service/docker/src/test/java/org/eclipse/jkube/kit/build/service/docker/auth/ecr/DockerRegistryAuthHandlerTest.java
@@ -21,198 +21,192 @@ import java.nio.file.Files;
 import java.util.Base64;
 import java.util.Map;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import org.eclipse.jkube.kit.build.api.auth.AuthConfig;
 import org.eclipse.jkube.kit.build.api.auth.RegistryAuthConfig;
 import org.eclipse.jkube.kit.build.service.docker.auth.DockerRegistryAuthHandler;
 import org.eclipse.jkube.kit.common.JsonFactory;
 import org.eclipse.jkube.kit.common.KitLogger;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import mockit.Mocked;
-import org.hamcrest.Matchers;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static java.util.Collections.singletonMap;
-import static org.hamcrest.CoreMatchers.startsWith;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author roland
  */
 public class DockerRegistryAuthHandlerTest {
 
+  @Mocked
+  private KitLogger log;
 
-    @Mocked
-    private KitLogger log;
+  private DockerRegistryAuthHandler handler;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+  @Before
+  public void setup() {
+    handler = new DockerRegistryAuthHandler(log);
+  }
 
-    private DockerRegistryAuthHandler handler;
+  @Test
+  public void testDockerAuthLogin() throws Exception {
+    executeWithTempHomeDir(homeDir -> {
+      checkDockerAuthLogin(homeDir, "https://index.docker.io/v1/", null);
+      checkDockerAuthLogin(homeDir, "localhost:5000", "localhost:5000");
+      checkDockerAuthLogin(homeDir, "https://localhost:5000", "localhost:5000");
+    });
+  }
 
-    @Before
-    public void setup() {
-        handler = new DockerRegistryAuthHandler(log);
+  @Test
+  public void testDockerLoginNoConfig() throws IOException {
+    executeWithTempHomeDir(dir -> {
+      AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", null, s -> s);
+      assertNull(config);
+    });
+  }
+
+  @Test
+  public void testDockerLoginFallsBackToAuthWhenCredentialHelperDoesNotMatchDomain() throws IOException {
+    executeWithTempHomeDir(homeDir -> {
+      writeDockerConfigJson(createDockerConfig(homeDir), null, singletonMap("registry1", "credHelper1-does-not-exist"));
+      AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "localhost:5000", s -> s);
+      verifyAuthConfig(config, "roland", "secret", "roland@jolokia.org");
+    });
+  }
+
+  @Test
+  public void testDockerLoginNoAuthConfigFoundWhenCredentialHelperDoesNotMatchDomainOrAuth() throws IOException {
+    executeWithTempHomeDir(homeDir -> {
+      writeDockerConfigJson(createDockerConfig(homeDir), null, singletonMap("registry1", "credHelper1-does-not-exist"));
+      AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "does-not-exist-either:5000", s -> s);
+      assertNull(config);
+    });
+  }
+
+  @Test
+  public void testDockerLoginSelectCredentialHelper() throws IOException {
+    executeWithTempHomeDir(homeDir -> {
+      writeDockerConfigJson(createDockerConfig(homeDir), "credsStore-does-not-exist",
+          singletonMap("registry1", "credHelper1-does-not-exist"));
+      RuntimeException exception = assertThrows(RuntimeException.class,
+          () -> handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "registry1", s -> s));
+      Assertions.assertThat(exception).getCause()
+          .isInstanceOf(IOException.class)
+          .hasMessageStartingWith("Failed to start 'docker-credential-credHelper1-does-not-exist version'");
+    });
+  }
+
+  @Test
+  public void testDockerLoginSelectCredentialsStore() throws IOException {
+    executeWithTempHomeDir(homeDir -> {
+      writeDockerConfigJson(createDockerConfig(homeDir), "credsStore-does-not-exist",
+          singletonMap("registry1", "credHelper1-does-not-exist"));
+      RuntimeException exception = assertThrows(RuntimeException.class,
+          () -> handler.create(RegistryAuthConfig.Kind.PUSH, "roland", null, s -> s));
+      Assertions.assertThat(exception).getCause()
+          .isInstanceOf(IOException.class)
+          .hasMessageStartingWith("Failed to start 'docker-credential-credsStore-does-not-exist version'");
+    });
+  }
+
+  @Test
+  public void testDockerLoginDefaultToCredentialsStore() throws IOException {
+
+    executeWithTempHomeDir(homeDir -> {
+      writeDockerConfigJson(createDockerConfig(homeDir), "credsStore-does-not-exist",
+          singletonMap("registry1", "credHelper1-does-not-exist"));
+      RuntimeException exception = assertThrows(RuntimeException.class,
+          () -> handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "registry2", s -> s));
+      Assertions.assertThat(exception).getCause()
+          .hasCauseInstanceOf(IOException.class)
+          .hasMessageStartingWith("Failed to start 'docker-credential-credsStore-does-not-exist version'");
+    });
+  }
+
+  private void executeWithTempHomeDir(HomeDirExecutor executor) throws IOException {
+    String userHome = System.getProperty("user.home");
+    try {
+      File tempDir = Files.createTempDirectory("d-m-p").toFile();
+      System.setProperty("user.home", tempDir.getAbsolutePath());
+      executor.exec(tempDir);
+    } finally {
+      System.setProperty("user.home", userHome);
     }
 
-    @Test
-    public void testDockerAuthLogin() throws Exception {
-        executeWithTempHomeDir(homeDir -> {
-            checkDockerAuthLogin(homeDir, "https://index.docker.io/v1/", null);
-            checkDockerAuthLogin(homeDir,"localhost:5000","localhost:5000");
-            checkDockerAuthLogin(homeDir,"https://localhost:5000","localhost:5000");
-        });
+  }
+
+  interface HomeDirExecutor {
+    void exec(File dir) throws IOException;
+  }
+
+  private void checkDockerAuthLogin(File homeDir, String configRegistry, String lookupRegistry)
+      throws IOException {
+
+    writeDockerConfigJson(createDockerConfig(homeDir), "roland", "secret", "roland@jolokia.org", configRegistry);
+    AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", lookupRegistry, s -> s);
+    verifyAuthConfig(config, "roland", "secret", "roland@jolokia.org");
+  }
+
+  private File createDockerConfig(File homeDir) {
+    File dockerDir = new File(homeDir, ".docker");
+    dockerDir.mkdirs();
+    return dockerDir;
+  }
+
+  private void writeDockerConfigJson(File dockerDir, String user, String password,
+      String email, String registry) throws IOException {
+    File configFile = new File(dockerDir, "config.json");
+
+    JsonObject config = new JsonObject();
+    addAuths(config, user, password, email, registry);
+
+    try (Writer writer = new FileWriter(configFile)) {
+      new Gson().toJson(config, writer);
+    }
+  }
+
+  private void writeDockerConfigJson(File dockerDir, String credsStore, Map<String, String> credHelpers) throws IOException {
+    File configFile = new File(dockerDir, "config.json");
+
+    JsonObject config = new JsonObject();
+    if (!credHelpers.isEmpty()) {
+      config.add("credHelpers", JsonFactory.newJsonObject(credHelpers));
     }
 
-    @Test
-    public void testDockerLoginNoConfig() throws IOException {
-        executeWithTempHomeDir(dir -> {
-            AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", null, s -> s);
-            assertNull(config);
-        });
+    if (credsStore != null) {
+      config.addProperty("credsStore", credsStore);
     }
 
-    @Test
-    public void testDockerLoginFallsBackToAuthWhenCredentialHelperDoesNotMatchDomain() throws IOException {
-        executeWithTempHomeDir(homeDir -> {
-            writeDockerConfigJson(createDockerConfig(homeDir), null, singletonMap("registry1", "credHelper1-does-not-exist"));
-            AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "localhost:5000", s -> s);
-            verifyAuthConfig(config,"roland","secret","roland@jolokia.org");
-        });
+    addAuths(config, "roland", "secret", "roland@jolokia.org", "localhost:5000");
+
+    try (Writer writer = new FileWriter(configFile)) {
+      new Gson().toJson(config, writer);
     }
+  }
 
-    @Test
-    public void testDockerLoginNoAuthConfigFoundWhenCredentialHelperDoesNotMatchDomainOrAuth() throws IOException {
-        executeWithTempHomeDir(homeDir -> {
-            writeDockerConfigJson(createDockerConfig(homeDir),null,singletonMap("registry1", "credHelper1-does-not-exist"));
-            AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "does-not-exist-either:5000", s -> s);
-            assertNull(config);
-        });
+  private void addAuths(JsonObject config, String user, String password, String email, String registry) {
+    JsonObject auths = new JsonObject();
+    JsonObject value = new JsonObject();
+    value.addProperty("auth", new String(Base64.getEncoder().encode((user + ":" + password).getBytes())));
+    value.addProperty("email", email);
+    auths.add(registry, value);
+    config.add("auths", auths);
+  }
+
+  private void verifyAuthConfig(AuthConfig config, String username, String password, String email) {
+    JsonObject params = new Gson().fromJson(new String(Base64.getDecoder().decode(config.toHeaderValue(log).getBytes())),
+        JsonObject.class);
+    assertEquals(username, params.get("username").getAsString());
+    assertEquals(password, params.get("password").getAsString());
+    if (email != null) {
+      assertEquals(email, params.get("email").getAsString());
     }
-
-    @Test
-    public void testDockerLoginSelectCredentialHelper() throws IOException {
-        executeWithTempHomeDir(homeDir -> {
-            writeDockerConfigJson(createDockerConfig(homeDir),"credsStore-does-not-exist",singletonMap("registry1", "credHelper1-does-not-exist"));
-            expectedException.expect(RuntimeException.class);
-            expectedException.expectCause(Matchers.<Throwable>allOf(
-                    instanceOf(IOException.class),
-                    hasProperty("message",startsWith("Failed to start 'docker-credential-credHelper1-does-not-exist version'"))
-                                                                   ));
-            handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "registry1", s->s);
-        });
-    }
-
-    @Test
-    public void testDockerLoginSelectCredentialsStore() throws IOException {
-        executeWithTempHomeDir(homeDir -> {
-            writeDockerConfigJson(createDockerConfig(homeDir),"credsStore-does-not-exist",singletonMap("registry1", "credHelper1-does-not-exist"));
-            expectedException.expect(RuntimeException.class);
-            expectedException.expectCause(Matchers.allOf(
-                    instanceOf(IOException.class),
-                    hasProperty("message",startsWith("Failed to start 'docker-credential-credsStore-does-not-exist version'"))
-                                                        ));
-            handler.create(RegistryAuthConfig.Kind.PUSH, "roland", null, s->s);
-        });
-    }
-
-    @Test
-    public void testDockerLoginDefaultToCredentialsStore() throws IOException {
-
-        executeWithTempHomeDir(homeDir -> {
-            writeDockerConfigJson(createDockerConfig(homeDir),"credsStore-does-not-exist",singletonMap("registry1", "credHelper1-does-not-exist"));
-            expectedException.expect(RuntimeException.class);
-            expectedException.expectCause(Matchers.allOf(
-                    instanceOf(IOException.class),
-                    hasProperty("message",startsWith("Failed to start 'docker-credential-credsStore-does-not-exist version'"))
-                                                        ));
-            handler.create(RegistryAuthConfig.Kind.PUSH, "roland", "registry2", s->s);
-        });
-    }
-
-    private void executeWithTempHomeDir(HomeDirExecutor executor) throws IOException {
-        String userHome = System.getProperty("user.home");
-        try {
-            File tempDir = Files.createTempDirectory("d-m-p").toFile();
-            System.setProperty("user.home", tempDir.getAbsolutePath());
-            executor.exec(tempDir);
-        } finally {
-            System.setProperty("user.home", userHome);
-        }
-
-    }
-
-    interface HomeDirExecutor {
-        void exec(File dir) throws IOException;
-    }
-
-    private void checkDockerAuthLogin(File homeDir, String configRegistry, String lookupRegistry)
-        throws IOException {
-
-        writeDockerConfigJson(createDockerConfig(homeDir), "roland", "secret", "roland@jolokia.org", configRegistry);
-        AuthConfig config = handler.create(RegistryAuthConfig.Kind.PUSH, "roland", lookupRegistry, s -> s);
-        verifyAuthConfig(config,"roland","secret","roland@jolokia.org");
-    }
-
-    private File createDockerConfig(File homeDir) {
-        File dockerDir = new File(homeDir, ".docker");
-        dockerDir.mkdirs();
-        return dockerDir;
-    }
-
-    private void writeDockerConfigJson(File dockerDir, String user, String password,
-                                       String email, String registry) throws IOException {
-        File configFile = new File(dockerDir, "config.json");
-
-        JsonObject config = new JsonObject();
-        addAuths(config,user,password,email,registry);
-
-        try (Writer writer = new FileWriter(configFile)){
-            new Gson().toJson(config, writer);
-        }
-    }
-
-    private void writeDockerConfigJson(File dockerDir, String credsStore, Map<String,String> credHelpers) throws IOException {
-        File configFile = new File(dockerDir, "config.json");
-
-        JsonObject config = new JsonObject();
-        if (!credHelpers.isEmpty()){
-            config.add("credHelpers", JsonFactory.newJsonObject(credHelpers));
-        }
-
-        if (credsStore!=null) {
-            config.addProperty("credsStore",credsStore);
-        }
-
-        addAuths(config,"roland","secret","roland@jolokia.org", "localhost:5000");
-
-        try (Writer writer = new FileWriter(configFile)){
-            new Gson().toJson(config, writer);
-        }
-    }
-
-    private void addAuths(JsonObject config, String user, String password, String email, String registry) {
-        JsonObject auths = new JsonObject();
-        JsonObject value = new JsonObject();
-        value.addProperty("auth", new String(Base64.getEncoder().encode((user + ":" + password).getBytes())));
-        value.addProperty("email",email);
-        auths.add(registry, value);
-        config.add("auths",auths);
-    }
-
-    private void verifyAuthConfig(AuthConfig config, String username, String password, String email) {
-        JsonObject params = new Gson().fromJson(new String(Base64.getDecoder().decode(config.toHeaderValue(log).getBytes())), JsonObject.class);
-        assertEquals(username,params.get("username").getAsString());
-        assertEquals(password,params.get("password").getAsString());
-        if (email != null) {
-            assertEquals(email, params.get("email").getAsString());
-        }
-    }
+  }
 
 }
