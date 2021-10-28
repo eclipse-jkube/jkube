ffb9b758b09bdcd4db27925b2933033a1f7e019a
Harald Steinmueller <harald.steinmueller@gmail.com>
2021-10-28 07:09:38 +0200

    Remove deprecated ExpectedException.none() in Tests (#1041)
    * Remove deprecated Rule in KeyStoreUtilTest
    
    Signed-off-by: hsteinmueller <harald.steinmueller@gmail.com>
    
    * Remove more deprecated Rule usages in Tests
    
    Signed-off-by: hsteinmueller <harald.steinmueller@gmail.com>
    
    * Revert changes to all Tests except KeyStoreUtilTest
    
    Signed-off-by: hsteinmueller <harald.steinmueller@gmail.com>

--- jkube-kit/build/service/docker/src/test/java/org/eclipse/jkube/kit/build/service/docker/access/KeyStoreUtilTest.java
+++ jkube-kit/build/service/docker/src/test/java/org/eclipse/jkube/kit/build/service/docker/access/KeyStoreUtilTest.java
@@ -18,20 +18,17 @@ import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Stas Sukhanov
  * @since 08.03.2017
  */
 public class KeyStoreUtilTest {
-
-  @Rule
-  public ExpectedException exception = ExpectedException.none();
 
   @Test
   public void createKeyStore() throws Exception {
@@ -45,10 +42,9 @@ public class KeyStoreUtilTest {
   }
 
   @Test
-  public void loadInvalidPrivateKey() throws Exception {
-    exception.expect(GeneralSecurityException.class);
-    exception.expectMessage("Cannot generate private key");
-    KeyStoreUtil.loadPrivateKey(getFile("keys/invalid.pem"));
+  public void loadInvalidPrivateKey() {
+    GeneralSecurityException exception = assertThrows(GeneralSecurityException.class, () -> KeyStoreUtil.loadPrivateKey(getFile("keys/invalid.pem")));
+    assertThat(exception).hasMessageContaining("Cannot generate private key");
   }
 
   static String getFile(String path) throws FileNotFoundException {
