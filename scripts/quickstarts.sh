80cd6866b23a49acdea12b2bdcd92d685cb73e20
Marc Nuri <marc@marcnuri.com>
2021-11-10 17:36:10 +0100

    feat: Improve Quickstart scripts to include Gradle projects

--- scripts/quickstarts.sh
+++ scripts/quickstarts.sh
@@ -15,23 +15,51 @@
 
 trap 'exit' ERR
 
-BASEDIR=$(dirname "$BASH_SOURCE")
+BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source "$BASEDIR/common.sh"
 QUICKSTARTS="$PROJECT_ROOT/quickstarts"
 
-function version() {
-  JKUBE_VERSION=$(getJKubeVersion)
+function mvnVersion() {
+  JKUBE_VERSION=$1
   echo "Updating quickstarts pom files to JKube $JKUBE_VERSION"
   cd "$QUICKSTARTS" || exit 1
   find . -type f -name "pom.xml" -print0 | xargs -0 -L 1 -P 1 -I{} sh -c \
     "mvn -f '{}' versions:set -DnewVersion=$JKUBE_VERSION -DgenerateBackupPoms=false || exit 255"
 }
 
-function package() {
-  echo "Packaging all quickstart projects (excluding sub-modules)"
+function gradleVersion() {
+  JKUBE_VERSION=$1
+  echo "Updating quickstarts Gradle files to JKube $JKUBE_VERSION"
+  cd "$QUICKSTARTS" || exit 1
+  find . -type f -name "build.gradle" \
+    -exec sed -i "s/id 'org.eclipse.jkube.kubernetes' version .*$/id 'org.eclipse.jkube.kubernetes' version '$JKUBE_VERSION'/g" {} \;            \
+    -exec sed -i "s/id 'org.eclipse.jkube.openshift' version .*$/id 'org.eclipse.jkube.openshift' version '$JKUBE_VERSION'/g" {} \;              \
+    -exec sed -i "s/id(\"org.eclipse.jkube.kubernetes\") version .*$/id(\"org.eclipse.jkube.kubernetes\") version \"${JKUBE_VERSION}\"/g" {} \;  \
+    -exec sed -i "s/id(\"org.eclipse.jkube.openshift\") version .*$/id(\"org.eclipse.jkube.openshift\") version \"${JKUBE_VERSION}\"/g" {} \;
+}
+
+function version() {
+  JKUBE_VERSION=$(getJKubeVersion)
+  mvnVersion "$JKUBE_VERSION"
+  gradleVersion "$JKUBE_VERSION"
+}
+
+function packageMaven() {
+  echo "Packaging all Maven quickstart projects (excluding sub-modules)"
   cd "$QUICKSTARTS" || exit 1
   find . -type f -name "pom.xml" -exec grep -q -z -v '../pom.xml</relativePath>' {} \; -print0 | \
     xargs -0 -L 1 -P 1 -I{} sh -c "mvn -f '{}' clean package || exit 255"
+}
+
+function packageGradle() {
+  echo "Packaging all Gradle quickstart projects (excluding sub-modules)"
+  cd "$QUICKSTARTS" || exit 1
+  find . -type f -name "build.gradle" -execdir ./gradlew build \;
+}
+
+function package() {
+  packageMaven
+  packageGradle
 }
 
 if declare -f "$1" > /dev/null ; then
@@ -42,3 +70,4 @@ else
   echo "'$1' is not a known function name" >&2
   exit 1
 fi
+
