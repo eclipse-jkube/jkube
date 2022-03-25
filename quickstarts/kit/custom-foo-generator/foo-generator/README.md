0058991159e70a1df72cca89cf457259175af69d
Rohan Kumar <rohaan@redhat.com>
2022-03-25 21:57:56 +0530

    chore (quickstarts/kit) : Add quickstart for providing user defined Custom Generator (#1336)
    Add an example of how to provide your own custom generator to gradle
    plugins.
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ quickstarts/kit/custom-foo-generator/foo-generator/README.md
@@ -0,0 +1,15 @@
+# Foo Generator
+
+A simple generator which adds an Environment variable `foo=fooval` to existing ImageConfiguration.
+
+## How to Build?
+```sh
+$ gradle clean build
+```
+
+## Publishing to local Maven repository
+In this quickstart we'll be publishing this generator to local maven repository. You can run this gradle task to achieve that:
+
+```sh
+$ gradle publishToMavenLocal
+```
---
+++ quickstarts/kit/custom-foo-generator/foo-generator/src/main/java/org/eclipse/jkube/quickstarts/gradle/generator/FooGenerator.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.quickstarts.gradle.generator;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.eclipse.jkube.generator.api.GeneratorContext;
+import org.eclipse.jkube.generator.api.support.BaseGenerator;
+import org.eclipse.jkube.kit.common.Configs;
+import org.eclipse.jkube.kit.config.image.ImageConfiguration;
+import org.eclipse.jkube.kit.config.image.build.BuildConfiguration;
+
+import java.util.List;
+
+public class FooGenerator extends BaseGenerator {
+
+  public FooGenerator(GeneratorContext context) {
+    super(context, "foo");
+  }
+
+  @AllArgsConstructor
+  public enum Config implements Configs.Config {
+    ENABLED("enabled", "true");
+
+    @Getter
+    protected String key;
+    @Getter(AccessLevel.PUBLIC)
+    protected String defaultValue;
+  }
+
+  @Override
+  public boolean isApplicable(List<ImageConfiguration> configs) {
+    return Boolean.parseBoolean(getConfig(Config.ENABLED));
+  }
+
+  @Override
+  public List<ImageConfiguration> customize(List<ImageConfiguration> existingConfigs, boolean prePackagePhase) {
+    for (ImageConfiguration imageConfiguration : existingConfigs) {
+      getContext().getLogger().info("Add Environment variable to ImageConfigurations");
+      imageConfiguration.setBuild(addFooEnvVariable(imageConfiguration.getBuildConfiguration()));
+    }
+    return existingConfigs;
+  }
+
+  private BuildConfiguration addFooEnvVariable(BuildConfiguration buildConfiguration) {
+    return buildConfiguration.toBuilder()
+        .putEnv("foo", "fooval")
+        .build();
+  }
+}
