05290a1d6a40fa811e8faeb5c0cbcef6fb87b7d0
baruKreddy <96629790+baruKreddy@users.noreply.github.com>
2022-04-04 22:48:18 +0530

    Running with Enclosed.class

--- jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/OpenshiftHelperStatusTest.java
+++ jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/OpenshiftHelperStatusTest.java
@@ -14,6 +14,7 @@
 package org.eclipse.jkube.kit.common.util;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -22,28 +23,99 @@ import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class OpenshiftHelperStatusTest {
-    @Parameterized.Parameters(name = "{0}: isFinished({1})={2}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                { "IsCompleteTrue", "Complete", true },
-                { "IsErrorTrue", "Error", true },
-                { "IsCancelledTrue", "Cancelled", true },
-                { "IsNotCompleteFalse", "not Complete", false }
-        });
+    @RunWith(Parameterized.class)
+    public static class Finished {
+        @Parameterized.Parameters(name = "{0}: isFinished({1})={2}")
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {"IsCompleteTrue", "Complete", true},
+                    {"IsErrorTrue", "Error", true},
+                    {"IsCancelledTrue", "Cancelled", true},
+                    {"IsNotCompleteFalse", "not Complete", false}
+            });
+        }
+
+        @Parameterized.Parameter
+        public String description;
+        @Parameterized.Parameter(1)
+        public String input;
+        @Parameterized.Parameter(2)
+        public Boolean expected;
+
+        @Test
+        public void testIsFinishedUsingParametrizedTest() {
+            assertThat(OpenshiftHelper.isFinished(input)).isEqualTo(expected);
+        }
+    }
+    @RunWith(Parameterized.class)
+    public static class Cancelled {
+        @Parameterized.Parameters(name = "{0}: isCancelled({1})={2}")
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {"IsCancelledTrue", "Cancelled", true},
+                    {"IsNotCancelledFalse", "not Cancelled", false}
+            });
+        }
+
+        @Parameterized.Parameter
+        public String description;
+        @Parameterized.Parameter(1)
+        public String input;
+        @Parameterized.Parameter(2)
+        public Boolean expected;
+
+        @Test
+        public void testIsCancelledUsingParametrizedTest() {
+            assertThat(OpenshiftHelper.isCancelled(input)).isEqualTo(expected);
+        }
+    }
+    @RunWith(Parameterized.class)
+    public static class Failed {
+        @Parameterized.Parameters(name = "{0}: isFailed({1})={2}")
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {"IsFailTrue", "Fail", true},
+                    {"IsErrorTrue", "Error", true},
+                    {"IsNullFalse","null",false}
+            });
+        }
+
+        @Parameterized.Parameter
+        public String description;
+        @Parameterized.Parameter(1)
+        public String input;
+        @Parameterized.Parameter(2)
+        public Boolean expected;
+
+        @Test
+        public void testIsFailedUsingParametrizedTest() {
+            assertThat(OpenshiftHelper.isFailed(input)).isEqualTo(expected);
+        }
+    }
+    @RunWith(Parameterized.class)
+    public static class Completed {
+        @Parameterized.Parameters(name = "{0}: isCompleted({1})={2}")
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {"IsCompleteTue", "Complete", true},
+                    {"IsNotCompleteFalse", "not Complete", false}
+            });
+        }
+
+        @Parameterized.Parameter
+        public String description;
+        @Parameterized.Parameter(1)
+        public String input;
+        @Parameterized.Parameter(2)
+        public Boolean expected;
+
+        @Test
+        public void testIsCompletedUsingParametrizedTest() {
+            assertThat(OpenshiftHelper.isCompleted(input)).isEqualTo(expected);
+        }
     }
 
-    @Parameterized.Parameter
-    public String description;
-    @Parameterized.Parameter(1)
-    public String input;
-    @Parameterized.Parameter(2)
-    public Boolean expected;
-
-    @Test
-    public void testUsingParametrizedTest() {
-        assertThat(OpenshiftHelper.isFinished(input)).isEqualTo(expected);
-    }
 
 }
