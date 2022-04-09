81bf58042cb608220c49d3e09d0eb1a0f5d52376
Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>
2022-04-09 17:10:22 +0530

    chore (quickstart/kit): Port Quarkus to Gradle (#1431)
    Signed-off-by: Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>

---
+++ quickstarts/gradle/quarkus/src/main/java/org/eclipse/jkube/quickstarts/gradle/CoolApplication.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+package org.eclipse.jkube.quickstarts.gradle;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class CoolApplication {
+
+  private final String applicationName;
+  private final String message;
+
+  public CoolApplication(String applicationName, String message) {
+    this.applicationName = applicationName;
+    this.message = message;
+  }
+
+  public String getApplicationName() {
+    return applicationName;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
