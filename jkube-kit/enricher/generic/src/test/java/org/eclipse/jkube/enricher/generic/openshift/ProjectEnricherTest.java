68216155b9ec0ee19d5f98021f2202b3015fb040
Devamparikh <devamparikh15@gmail.com>
2022-04-19 10:14:00 +0530

    test (jkube-kit/enricher): Add ProjectEnricherTest for ProjectEnricher(enricher/generic) (#1441)

--- jkube-kit/enricher/generic/src/test/java/org/eclipse/jkube/enricher/generic/openshift/ProjectEnricherTest.java
+++ jkube-kit/enricher/generic/src/test/java/org/eclipse/jkube/enricher/generic/openshift/ProjectEnricherTest.java
@@ -1,10 +1,20 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
 package org.eclipse.jkube.enricher.generic.openshift;
 
-import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
-import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.openshift.api.model.Project;
-import org.eclipse.jkube.kit.common.JavaProject;
-import org.eclipse.jkube.kit.common.KitLogger;
 import org.eclipse.jkube.kit.config.resource.PlatformMode;
 import org.eclipse.jkube.kit.config.resource.ResourceConfig;
 import org.eclipse.jkube.kit.enricher.api.EnricherContext;
@@ -14,35 +24,34 @@ import org.junit.Test;
 import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
 
 public class ProjectEnricherTest {
 
     private EnricherContext context;
 
     public void setExpectations(Properties properties, ResourceConfig resourceConfig) {
-        context = JKubeEnricherContext.builder()
-                .log(new KitLogger.SilentLogger())
-                .resources(resourceConfig)
-                .project(JavaProject.builder()
-                        .properties(properties)
-                        .groupId("group")
-                        .artifactId("artifact-id")
-                        .build())
-                .build();
+        context = mock(JKubeEnricherContext.class, RETURNS_DEEP_STUBS);
     }
 
     @Test
     public void convertNamespaceResourceToProject() {
         // Given
         Properties properties = new Properties();
-        properties.put("jkube.enricher.jkube-namespace.namespace", "example");
         setExpectations(properties, new ResourceConfig());
         final KubernetesListBuilder klb = new KubernetesListBuilder();
+        klb.addToItems(new NamespaceBuilder().withNewMetadata().withName("foo").endMetadata());
         // When
         new ProjectEnricher((JKubeEnricherContext) context).create(PlatformMode.openshift, klb);
         // Then
-        assertThat(klb.build().getItems()).isEmpty();
+        assertThat(klb.build())
+                .extracting(KubernetesList::getItems)
+                .asList()
+                .hasSize(1)
+                .first()
+                .isInstanceOf(Project.class)
+                .hasFieldOrPropertyWithValue("metadata.name", "foo");
     }
 
 }
