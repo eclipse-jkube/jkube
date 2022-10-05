c0657522ca0b2f1cf88c7ced1afcb95b40fd7d38
Abulmajd <mabulmagd55@gmail.com>
2022-10-05 14:06:55 +0200

    chore(test): Add unit test for KindFilenameMapperUtil (#1544)
    * chore(test): KindFilenameMapperUtil unit test
    
    * chore(test): KindFilenameMapperUtil unit test cleanup
    
    * Signing off
    
    Signed-off-by: mmajd <mabulmagd55@gmail.com>
    
    * change: remove line separators and use new line charactoer
    
    * Change import Test from use jupiter api and remove SystemMock import
    
    * change(KindFilenameMapperTest): remove dependency on AssciiMapper and PropertiesMapper
    
    * change(KindFilenameMapperTest): remove dependency on AssciiMapper and PropertiesMapper
    
    * (chore/KindFilenameMapperUtilTest) refactoring and removing unnecessary files
    
    * review: to squash
    
    
    Signed-off-by: mmajd <mabulmagd55@gmail.com>

---
+++ jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/KindFilenameMapperUtilTest.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.kit.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+class KindFilenameMapperUtilTest {
+    @Test
+    void shouldLoadMappings() {
+        // given
+
+        // source: test/resources/**/kind-file-type-mappings-default.ascii
+        // source: test/resources/**/kind-file-type-mappings-default.properties
+
+        // when
+        Map<String, List<String>> defaultMappings = KindFilenameMapperUtil.loadMappings();
+
+        // then
+        assertThat(defaultMappings)
+          .containsOnly(
+            entry("BuildConfig", Arrays.asList("bc", "buildconfig")),
+            entry("ClusterRole", Arrays.asList("cr", "crole", "clusterrole")),
+            entry("ConfigMap", Arrays.asList("cm", "configmap")),
+            entry("CronJob", Arrays.asList("cj", "cronjob")),
+            entry("Pod", Arrays.asList("pd", "pod"))
+          );
+    }
+}
