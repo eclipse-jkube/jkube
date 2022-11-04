f1af5973c7f821f926b2068960341f789c8aa228
Marc Nuri <marc@marcnuri.com>
2022-11-04 11:11:03 +0100

    test(docker-build): additional test-cases for HTTP response processing
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- jkube-kit/build/service/docker/src/test/java/org/eclipse/jkube/kit/build/service/docker/access/hc/HcChunkedResponseHandlerWrapperTest.java
+++ jkube-kit/build/service/docker/src/test/java/org/eclipse/jkube/kit/build/service/docker/access/hc/HcChunkedResponseHandlerWrapperTest.java
@@ -13,75 +13,105 @@
  */
 package org.eclipse.jkube.kit.build.service.docker.access.hc;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import com.google.gson.JsonObject;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicHttpResponse;
+import org.eclipse.jkube.kit.build.service.docker.access.chunked.EntityStreamReaderUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import org.eclipse.jkube.kit.build.service.docker.access.chunked.EntityStreamReaderUtil;
-
-import mockit.Expectations;
-import mockit.Mocked;
-import mockit.Verifications;
-import org.apache.http.Header;
-import org.apache.http.HttpResponse;
-import org.apache.http.message.BasicHeader;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 @SuppressWarnings("unused")
 class HcChunkedResponseHandlerWrapperTest {
 
-  @Mocked
-  private EntityStreamReaderUtil.JsonEntityResponseHandler handler;
-  @Mocked
-  private HttpResponse response;
-  @Mocked
-  private EntityStreamReaderUtil entityStreamReaderUtil;
-
   private Header[] headers;
+  private TestJsonEntityResponseHandler handler;
   private HcChunkedResponseHandlerWrapper hcChunkedResponseHandlerWrapper;
 
   @BeforeEach
   void setUp() {
+    handler = new TestJsonEntityResponseHandler();
     hcChunkedResponseHandlerWrapper = new HcChunkedResponseHandlerWrapper(handler);
   }
 
   @Test
-  void handleResponseWithJsonResponse() throws IOException {
-    givenResponseHeaders(new BasicHeader("ConTenT-Type", "application/json; charset=UTF-8"));
+  void handleResponseWithInvalidStatusAndJsonBody() throws IOException {
+    final HttpResponse response = response(400, "WRONG!!",
+      new StringEntity("{}"));
     hcChunkedResponseHandlerWrapper.handleResponse(response);
-    verifyProcessJsonStream(1);
+    // TODO: Maybe we should propagate the status in some way
+    assertThat(handler.processedObject).isNotNull();
   }
 
   @Test
-  void handleResponseWithTextPlainResponse() throws IOException {
-    givenResponseHeaders(new BasicHeader("Content-Type", "text/plain"));
-    assertThrows(IllegalArgumentException.class, () -> {
-      hcChunkedResponseHandlerWrapper.handleResponse(response);
-    });
+  void handleResponseWithJsonContentTypeAndJsonBody() throws IOException {
+    final HttpResponse response = response(200, "OK",
+      new StringEntity("{\"field\":\"value\"}"),
+      new BasicHeader("ConTenT-Type", "application/json; charset=UTF-8"));
+    hcChunkedResponseHandlerWrapper.handleResponse(response);
+    assertThat(handler.processedObject)
+      .returns("value", jo -> jo.get("field").getAsString());
+  }
+  @Test
+  void handleResponseWithJsonContentTypeAndInvalidBody() throws IOException {
+    final HttpResponse response = response(200, "OK",
+      new StringEntity("This is not a JSON string"),
+      new BasicHeader("ConTenT-Type", "application/json"));
+    assertThatIllegalStateException()
+      .isThrownBy(() -> hcChunkedResponseHandlerWrapper.handleResponse(response))
+      .withMessageStartingWith("Not a JSON Object:");
   }
 
   @Test
-  void handleResponseWithNoContentTypeAsForPodman() throws IOException {
-    givenResponseHeaders();
+  void handleResponseWithTextPlainAndJsonBody() throws IOException {
+    final HttpResponse response = response(200, "OK",
+      new StringEntity("{\"field\":\"value\"}"),
+      new BasicHeader("ConTenT-Type", "text/plain"));
     hcChunkedResponseHandlerWrapper.handleResponse(response);
-    verifyProcessJsonStream(1);
+    assertThat(handler.processedObject)
+      .returns("value", jo -> jo.get("field").getAsString());
   }
 
-  private void givenResponseHeaders(Header... headers) {
-    // @formatter:off
-    new Expectations() {{
-      response.getAllHeaders(); result = headers;
-    }};
-    // @formatter:on
+  @Test
+  void handleResponseWithNoContentTypeAndJsonBody() throws IOException {
+    final HttpResponse response = response(200, "OK",
+      new StringEntity("{\"field\":\"value\"}"));
+    hcChunkedResponseHandlerWrapper.handleResponse(response);
+    assertThat(handler.processedObject)
+      .returns("value", jo -> jo.get("field").getAsString());
   }
 
-  @SuppressWarnings("AccessStaticViaInstance")
-  private void verifyProcessJsonStream(int timesCalled) throws IOException {
-    // @formatter:off
-    new Verifications() {{
-      entityStreamReaderUtil.processJsonStream(handler, response.getEntity().getContent()); times = timesCalled;
-    }};
-    // @formatter:on
+  private static HttpResponse response(int code, String reason, HttpEntity entity, Header... headers) {
+    final BasicHttpResponse response = new BasicHttpResponse(
+      new ProtocolVersion("HTTP", 1, 1), code, reason);
+    response.setEntity(entity);
+    response.setHeaders(headers);
+    return response;
+  }
+
+  private static final class TestJsonEntityResponseHandler implements EntityStreamReaderUtil.JsonEntityResponseHandler {
+
+    private JsonObject processedObject;
+    @Override
+    public void process(JsonObject toProcess) {
+      processedObject = toProcess;
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+    }
   }
 }
