f6c0868c473ff3d161dabb447540a0bb367dfc6e
chambora1312 <jendrikbadde@gmail.com>
2022-12-26 21:41:19 +0100

    File[] resources and List<ValidationRule> are now final/ issue#1866

--- jkube-kit/common/src/main/java/org/eclipse/jkube/kit/common/util/validator/ResourceValidator.java
+++ jkube-kit/common/src/main/java/org/eclipse/jkube/kit/common/util/validator/ResourceValidator.java
@@ -53,9 +53,9 @@ public class ResourceValidator {
 
     public static final String SCHEMA_JSON = "schema/validation-schema.json";
     private KitLogger log;
-    private File[] resources;
+    private final File[] resources;
     private ResourceClassifier target = ResourceClassifier.KUBERNETES;
-    private List<ValidationRule> ignoreValidationRules = new ArrayList<>();
+    private final List<ValidationRule> ignoreValidationRules = new ArrayList<>();
 
     /**
      * @param inputFile File/Directory path of resource descriptors
