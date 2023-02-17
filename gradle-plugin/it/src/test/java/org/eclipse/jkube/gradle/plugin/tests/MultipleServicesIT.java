d1c6c7aad47823609f737f91a99319939abcea5c
Rohan Kumar <rohaan@redhat.com>
2023-02-17 15:51:15 +0530

    test (gradle-plugin/it) : Add test to verify ProjectLabelEnricher doesn't override fragment defined labels (#1669)
    Add an integration test containing multiple services `first-service` and
    `second-service` Services provided via fragments in `src/main/jkube`
    which have their custom `app` labels defined in metadata and selectors.
    
    Test verifies that labels provided in fragments stay intact and are not modified
    during JKube's resource phase execution.
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/MultipleServicesIT.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.tests;
+
+import net.minidev.json.parser.ParseException;
+import org.eclipse.jkube.kit.common.ResourceVerify;
+import org.gradle.testkit.runner.BuildResult;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class MultipleServicesIT {
+  @RegisterExtension
+  private final ITGradleRunnerExtension gradleRunner = new ITGradleRunnerExtension();
+
+  @Test
+  void k8sResource_whenRun_generatesK8sManifests() throws IOException, ParseException {
+    // When
+    final BuildResult result = gradleRunner.withITProject("multiple-services")
+        .withArguments("build", "k8sResource", "--stacktrace")
+        .build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultKubernetesResourceFile(),
+        gradleRunner.resolveFile("expected", "kubernetes.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("jkube-controller: Adding a default Deployment")
+        .contains("jkube-service: Adding a default service")
+        .contains("jkube-service-discovery: Using first mentioned service port '8080' ")
+        .contains("jkube-revision-history: Adding revision history limit to 2");
+  }
+
+  @Test
+  void ocResource_whenRun_generatesOpenShiftManifests() throws IOException, ParseException {
+    // When
+    final BuildResult result = gradleRunner.withITProject("multiple-services")
+        .withArguments("build", "ocResource", "--stacktrace")
+        .build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultOpenShiftResourceFile(),
+        gradleRunner.resolveFile("expected", "openshift.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("jkube-controller: Adding a default Deployment")
+        .contains("jkube-service: Adding a default service")
+        .contains("jkube-openshift-deploymentconfig: Converting Deployment to DeploymentConfig")
+        .contains("jkube-service-discovery: Using first mentioned service port '8080' ")
+        .contains("jkube-revision-history: Adding revision history limit to 2");
+  }
+}
