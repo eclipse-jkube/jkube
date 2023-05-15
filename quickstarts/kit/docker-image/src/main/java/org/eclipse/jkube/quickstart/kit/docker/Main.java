8a653aff6f7e1adabff43315e5ae58cc08fe8f1a
Marc Nuri <marc@marcnuri.com>
2023-05-15 15:44:45 +0200

    quickstarts: fix: Arguments class was moved (#1969)
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- quickstarts/kit/docker-image/src/main/java/org/eclipse/jkube/quickstart/kit/docker/Main.java
+++ quickstarts/kit/docker-image/src/main/java/org/eclipse/jkube/quickstart/kit/docker/Main.java
@@ -18,6 +18,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.eclipse.jkube.kit.build.service.docker.DockerServiceHub;
+import org.eclipse.jkube.kit.common.Arguments;
 import org.eclipse.jkube.kit.config.image.ImageConfiguration;
 import org.eclipse.jkube.kit.config.image.RunImageConfiguration;
 import org.eclipse.jkube.kit.common.Assembly;
@@ -26,7 +27,6 @@ import org.eclipse.jkube.kit.common.AssemblyFileSet;
 import org.eclipse.jkube.kit.common.JavaProject;
 import org.eclipse.jkube.kit.common.KitLogger;
 import org.eclipse.jkube.kit.common.JKubeConfiguration;
-import org.eclipse.jkube.kit.config.image.build.Arguments;
 import org.eclipse.jkube.kit.config.image.build.BuildConfiguration;
 import org.eclipse.jkube.kit.config.resource.RuntimeMode;
 import org.eclipse.jkube.kit.config.service.BuildServiceConfig;
--- quickstarts/kit/dynamic-docker-image-file-multi-layer/src/main/java/org/eclipse/jkube/quickstart/kit/docker/dynamic/DynamicDockerfileGenerator.java
+++ quickstarts/kit/dynamic-docker-image-file-multi-layer/src/main/java/org/eclipse/jkube/quickstart/kit/docker/dynamic/DynamicDockerfileGenerator.java
@@ -14,10 +14,10 @@
 package org.eclipse.jkube.quickstart.kit.docker.dynamic;
 
 import org.apache.commons.io.FileUtils;
+import org.eclipse.jkube.kit.common.Arguments;
 import org.eclipse.jkube.kit.common.AssemblyConfiguration;
 import org.eclipse.jkube.kit.common.KitLogger;
 import org.eclipse.jkube.kit.config.image.ImageConfiguration;
-import org.eclipse.jkube.kit.config.image.build.Arguments;
 import org.eclipse.jkube.kit.config.image.build.BuildConfiguration;
 import org.eclipse.jkube.kit.config.image.build.DockerFileBuilder;
 
