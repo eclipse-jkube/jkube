c7e7818de76c1239359d8e19b748e393d1cdd487
Rohan Kumar <rohaan@redhat.com>
2023-05-31 17:01:24 +0530

    test (gradle-plugin/it) : Add test for setting ServiceAccount name in generated controller (#2187)
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/ServiceAccountNameViaGroovyDSLIT.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.tests;
+
+import net.minidev.json.parser.ParseException;
+import org.eclipse.jkube.kit.common.ResourceVerify;
+import org.gradle.testkit.runner.BuildResult;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class ServiceAccountNameViaGroovyDSLIT {
+  @RegisterExtension
+  private final ITGradleRunnerExtension gradleRunner = new ITGradleRunnerExtension();
+
+  static Stream<Arguments> testInput() {
+    return Stream.of(
+//        arguments("no-fragment", new String[] {}),
+//        arguments("unrelated-fragment", new String[] {"-Pjkube.resourceDir=./regularFragmentDir"}),
+        arguments("fragment-overriding-serviceaccount", new String[] {"-Pjkube.resourceDir=./overridingServiceAccountNameFragmentDir"})
+    );
+  }
+
+  @ParameterizedTest(name = "k8sResource with {0} configures ServiceAccount in generated Deployment")
+  @MethodSource("testInput")
+  void k8sResource_whenRun_shouldAddServiceAccountInDeployment(String expectedDir, String[] arguments) throws IOException, ParseException {
+    // When
+    List<String> gradleArgs = new ArrayList<>(Arrays.asList(arguments));
+    gradleArgs.add("k8sResource");
+    gradleArgs.add("--stacktrace");
+    final BuildResult result = gradleRunner.withITProject("serviceaccountname-via-groovy-dsl")
+        .withArguments(gradleArgs.toArray(new String[0]))
+        .build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultKubernetesResourceFile(),
+        gradleRunner.resolveFile("expected", expectedDir, "kubernetes.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("Using resource templates from")
+        .contains("Adding revision history limit to 2")
+        .contains("validating");
+  }
+
+  @ParameterizedTest(name = "ocResource with {0} configures ServiceAccount in generated DeploymentConfig")
+  @MethodSource("testInput")
+  void ocResource_whenRun_shouldAddServiceAccountInDeploymentConfig(String expectedDir, String[] arguments) throws IOException, ParseException {
+    // When
+    List<String> gradleArgs = new ArrayList<>(Arrays.asList(arguments));
+    gradleArgs.add("ocResource");
+    gradleArgs.add("--stacktrace");
+    final BuildResult result = gradleRunner.withITProject("serviceaccountname-via-groovy-dsl")
+        .withArguments(gradleArgs.toArray(new String[0]))
+        .build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultOpenShiftResourceFile(),
+        gradleRunner.resolveFile("expected", expectedDir, "openshift.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("Using resource templates from")
+        .contains("Adding revision history limit to 2")
+        .contains("validating");
+  }
+}
