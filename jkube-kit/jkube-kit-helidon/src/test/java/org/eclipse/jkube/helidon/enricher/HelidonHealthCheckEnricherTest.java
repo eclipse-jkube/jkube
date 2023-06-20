c57a230d2822a71cf3044ab322e29b279350e1db
Marc Nuri <marc@marcnuri.com>
2023-06-20 07:09:30 +0200

    review:refactor: helidon tests don't need mockito
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- jkube-kit/jkube-kit-helidon/src/test/java/org/eclipse/jkube/helidon/enricher/HelidonHealthCheckEnricherTest.java
+++ jkube-kit/jkube-kit-helidon/src/test/java/org/eclipse/jkube/helidon/enricher/HelidonHealthCheckEnricherTest.java
@@ -19,26 +19,22 @@ import io.fabric8.kubernetes.api.model.Probe;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import org.assertj.core.api.AssertionsForInterfaceTypes;
-import org.eclipse.jkube.helidon.HelidonUtils;
 import org.eclipse.jkube.kit.common.Dependency;
 import org.eclipse.jkube.kit.common.JavaProject;
+import org.eclipse.jkube.kit.common.KitLogger;
 import org.eclipse.jkube.kit.config.resource.PlatformMode;
 import org.eclipse.jkube.kit.config.resource.ProcessorConfig;
 import org.eclipse.jkube.kit.enricher.api.JKubeEnricherContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import java.io.File;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Answers.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class HelidonHealthCheckEnricherTest {
   private JKubeEnricherContext context;
@@ -49,31 +45,31 @@ class HelidonHealthCheckEnricherTest {
   @BeforeEach
   void setup() {
     properties = new Properties();
-    ProcessorConfig processorConfig = new ProcessorConfig();
     klb = new KubernetesListBuilder();
     klb.addToItems(new DeploymentBuilder()
-        .editOrNewSpec()
-        .editOrNewTemplate()
-        .editOrNewMetadata()
-        .withName("template-name")
-        .endMetadata()
-        .editOrNewSpec()
-        .addNewContainer()
-        .withImage("container/image")
-        .endContainer()
-        .endSpec()
-        .endTemplate()
-        .endSpec()
-        .build());
-
-    context = mock(JKubeEnricherContext.class, RETURNS_DEEP_STUBS);
-    javaProject = mock(JavaProject.class, RETURNS_DEEP_STUBS);
-    when(context.getProject()).thenReturn(javaProject);
-    when(context.getProperties()).thenReturn(properties);
-    when(context.getConfiguration().getProcessorConfig()).thenReturn(processorConfig);
-    when(javaProject.getProperties()).thenReturn(properties);
-    when(javaProject.getBaseDirectory()).thenReturn(new File("/tmp/ignore"));
-    when(javaProject.getOutputDirectory()).thenReturn(new File("/tmp/ignore"));
+      .editOrNewSpec()
+      .editOrNewTemplate()
+      .editOrNewMetadata()
+      .withName("template-name")
+      .endMetadata()
+      .editOrNewSpec()
+      .addNewContainer()
+      .withImage("container/image")
+      .endContainer()
+      .endSpec()
+      .endTemplate()
+      .endSpec()
+      .build());
+    javaProject = JavaProject.builder()
+      .properties(properties)
+      .outputDirectory(new File("/tmp/ignored"))
+      .dependenciesWithTransitive(new ArrayList<>())
+      .build();
+    context = JKubeEnricherContext.builder()
+      .log(new KitLogger.SilentLogger())
+      .project(javaProject)
+      .processorConfig(new ProcessorConfig())
+      .build();
   }
 
   @Test
@@ -103,88 +99,90 @@ class HelidonHealthCheckEnricherTest {
 
   @Test
   void create_withMicroprofileDependencyAndMicroprofileFeatureEnabled_shouldAddProbes() {
-    try (MockedStatic<HelidonUtils> mockStatic = Mockito.mockStatic(HelidonUtils.class)) {
-      // Given
-      mockStatic.when(() -> HelidonUtils.hasHelidonHealthDependency(javaProject)).thenReturn(true);
-      withMicroprofileDependency("5.0");
-      HelidonHealthCheckEnricher helidonHealthCheckEnricher = new HelidonHealthCheckEnricher(context);
-      // When
-      helidonHealthCheckEnricher.create(PlatformMode.kubernetes, klb);
-      // Then
-      assertProbesAdded("HTTP", "/health/live", "HTTP", "/health/ready", "HTTP", "/health/started");
-    }
+    // Given
+    withHelidonHealth();
+    withMicroprofileDependency("5.0");
+    HelidonHealthCheckEnricher helidonHealthCheckEnricher = new HelidonHealthCheckEnricher(context);
+    // When
+    helidonHealthCheckEnricher.create(PlatformMode.kubernetes, klb);
+    // Then
+    assertProbesAdded("HTTP", "/health/live", "HTTP", "/health/ready", "HTTP", "/health/started");
   }
 
   @Test
   void create_withLegacyMicroprofileDependencyAndMicroprofileFeatureEnabled_shouldOnlyAddLivenessReadinessProbes() {
-    try (MockedStatic<HelidonUtils> mockStatic = Mockito.mockStatic(HelidonUtils.class)) {
-      // Given
-      mockStatic.when(() -> HelidonUtils.hasHelidonHealthDependency(javaProject)).thenReturn(true);
-      withMicroprofileDependency("2.2");
-      HelidonHealthCheckEnricher helidonHealthCheckEnricher = new HelidonHealthCheckEnricher(context);
-      // When
-      helidonHealthCheckEnricher.create(PlatformMode.kubernetes, klb);
-      // Then
-      assertProbesAdded("HTTP", "/health/live", "HTTP", "/health/ready", null, null);
-      assertThat(getFirstContainerFromDeployment())
-          .extracting(Container::getStartupProbe)
-          .isNull();
-    }
+    // Given
+    withHelidonHealth();
+    withMicroprofileDependency("2.2");
+    HelidonHealthCheckEnricher helidonHealthCheckEnricher = new HelidonHealthCheckEnricher(context);
+    // When
+    helidonHealthCheckEnricher.create(PlatformMode.kubernetes, klb);
+    // Then
+    assertProbesAdded("HTTP", "/health/live", "HTTP", "/health/ready", null, null);
+    assertThat(getFirstContainerFromDeployment())
+      .extracting(Container::getStartupProbe)
+      .isNull();
   }
 
   @Test
   void create_withMicroprofileEnabledAndEnricherConfiguration_shouldProbesAsConfigured() {
-    try (MockedStatic<HelidonUtils> mockStatic = Mockito.mockStatic(HelidonUtils.class)) {
-      // Given
-      mockStatic.when(() -> HelidonUtils.hasHelidonHealthDependency(javaProject)).thenReturn(true);
-      withMicroprofileDependency("5.0");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.scheme", "HTTPS");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.port", "8080");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessPath", "/custom/health/live");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessFailureThreshold", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessSuccessThreshold", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessInitialDelay", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessPeriodSeconds", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessPath", "/custom/health/ready");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessFailureThreshold", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessSuccessThreshold", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessInitialDelay", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessPeriodSeconds", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.startupPath", "/custom/health/startup");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.startupFailureThreshold", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.startupSuccessThreshold", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.startupInitialDelay", "5");
-      properties.put("jkube.enricher.jkube-healthcheck-helidon.startupPeriodSeconds", "5");
-      HelidonHealthCheckEnricher helidonHealthCheckEnricher = new HelidonHealthCheckEnricher(context);
-      // When
-      helidonHealthCheckEnricher.create(PlatformMode.kubernetes, klb);
-      // Then
-      assertProbesAdded("HTTPS", "/custom/health/live", "HTTPS", "/custom/health/ready", "HTTPS", "/custom/health/startup");
-      assertThat(getFirstContainerFromDeployment())
-          .hasFieldOrPropertyWithValue("livenessProbe.failureThreshold", 5)
-          .hasFieldOrPropertyWithValue("livenessProbe.successThreshold", 5)
-          .hasFieldOrPropertyWithValue("livenessProbe.initialDelaySeconds", 5)
-          .hasFieldOrPropertyWithValue("livenessProbe.periodSeconds", 5)
-          .hasFieldOrPropertyWithValue("livenessProbe.httpGet.port.IntVal", 8080)
-          .hasFieldOrPropertyWithValue("readinessProbe.failureThreshold", 5)
-          .hasFieldOrPropertyWithValue("readinessProbe.successThreshold", 5)
-          .hasFieldOrPropertyWithValue("readinessProbe.initialDelaySeconds", 5)
-          .hasFieldOrPropertyWithValue("readinessProbe.periodSeconds", 5)
-          .hasFieldOrPropertyWithValue("readinessProbe.httpGet.port.IntVal", 8080)
-          .hasFieldOrPropertyWithValue("startupProbe.failureThreshold", 5)
-          .hasFieldOrPropertyWithValue("startupProbe.successThreshold", 5)
-          .hasFieldOrPropertyWithValue("startupProbe.initialDelaySeconds", 5)
-          .hasFieldOrPropertyWithValue("startupProbe.periodSeconds", 5)
-          .hasFieldOrPropertyWithValue("startupProbe.httpGet.port.IntVal", 8080);
-    }
+    // Given
+    withHelidonHealth();
+    withMicroprofileDependency("5.0");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.scheme", "HTTPS");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.port", "8080");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessPath", "/custom/health/live");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessFailureThreshold", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessSuccessThreshold", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessInitialDelay", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.livenessPeriodSeconds", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessPath", "/custom/health/ready");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessFailureThreshold", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessSuccessThreshold", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessInitialDelay", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.readinessPeriodSeconds", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.startupPath", "/custom/health/startup");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.startupFailureThreshold", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.startupSuccessThreshold", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.startupInitialDelay", "5");
+    properties.put("jkube.enricher.jkube-healthcheck-helidon.startupPeriodSeconds", "5");
+    HelidonHealthCheckEnricher helidonHealthCheckEnricher = new HelidonHealthCheckEnricher(context);
+    // When
+    helidonHealthCheckEnricher.create(PlatformMode.kubernetes, klb);
+    // Then
+    assertProbesAdded("HTTPS", "/custom/health/live", "HTTPS", "/custom/health/ready", "HTTPS", "/custom/health/startup");
+    assertThat(getFirstContainerFromDeployment())
+      .hasFieldOrPropertyWithValue("livenessProbe.failureThreshold", 5)
+      .hasFieldOrPropertyWithValue("livenessProbe.successThreshold", 5)
+      .hasFieldOrPropertyWithValue("livenessProbe.initialDelaySeconds", 5)
+      .hasFieldOrPropertyWithValue("livenessProbe.periodSeconds", 5)
+      .hasFieldOrPropertyWithValue("livenessProbe.httpGet.port.IntVal", 8080)
+      .hasFieldOrPropertyWithValue("readinessProbe.failureThreshold", 5)
+      .hasFieldOrPropertyWithValue("readinessProbe.successThreshold", 5)
+      .hasFieldOrPropertyWithValue("readinessProbe.initialDelaySeconds", 5)
+      .hasFieldOrPropertyWithValue("readinessProbe.periodSeconds", 5)
+      .hasFieldOrPropertyWithValue("readinessProbe.httpGet.port.IntVal", 8080)
+      .hasFieldOrPropertyWithValue("startupProbe.failureThreshold", 5)
+      .hasFieldOrPropertyWithValue("startupProbe.successThreshold", 5)
+      .hasFieldOrPropertyWithValue("startupProbe.initialDelaySeconds", 5)
+      .hasFieldOrPropertyWithValue("startupProbe.periodSeconds", 5)
+      .hasFieldOrPropertyWithValue("startupProbe.httpGet.port.IntVal", 8080);
   }
 
   private void withMicroprofileDependency(String microProfileVersion) {
-    when(javaProject.getDependenciesWithTransitive()).thenReturn(Collections.singletonList(Dependency.builder()
-        .groupId("org.eclipse.microprofile")
-        .artifactId("microprofile")
-        .version(microProfileVersion)
-        .build()));
+    final List<Dependency> deps = new ArrayList<>(javaProject.getDependencies());
+    deps.add(Dependency.builder()
+      .groupId("org.eclipse.microprofile").artifactId("microprofile").version(microProfileVersion).build());
+    javaProject.setDependencies(deps);
+    javaProject.setDependenciesWithTransitive(deps);
+  }
+
+  private void withHelidonHealth() {
+    final List<Dependency> deps = new ArrayList<>(javaProject.getDependencies());
+    deps.add(Dependency.builder()
+      .groupId("io.helidon.health").artifactId("helidon-health").build());
+    javaProject.setDependencies(deps);
+    javaProject.setDependenciesWithTransitive(deps);
   }
 
   private void assertProbesAdded(String livenessScheme, String livenessPath, String readyScheme, String readyPath, String startedScheme, String startedPath) {
@@ -204,8 +202,8 @@ class HelidonHealthCheckEnricherTest {
   private void assertProbe(Supplier<Probe> probeSupplier, String scheme, String path) {
     if (scheme != null && path != null) {
       assertThat(probeSupplier.get())
-          .hasFieldOrPropertyWithValue("httpGet.scheme", scheme)
-          .hasFieldOrPropertyWithValue("httpGet.path", path);
+        .hasFieldOrPropertyWithValue("httpGet.scheme", scheme)
+        .hasFieldOrPropertyWithValue("httpGet.path", path);
     }
   }
 
