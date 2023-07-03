e82dcd7e963718021946f589cb7553d2cb5d23fa
Marc Nuri <marc@marcnuri.com>
2023-07-03 15:52:50 +0200

    fix: fragment group-version-kind completed only if necessary
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

---
+++ gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/FragmentsIT.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.tests;
+
+import org.eclipse.jkube.kit.common.ResourceVerify;
+import org.gradle.testkit.runner.BuildResult;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FragmentsIT {
+
+  @RegisterExtension
+  public final ITGradleRunnerExtension gradleRunner = new ITGradleRunnerExtension();
+
+  @Test
+  void k8sResource_withCustomMappings() throws Exception {
+    // When
+    final BuildResult result = gradleRunner.withITProject("fragments-custom-mapping")
+      .withArguments("k8sResource").build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultKubernetesResourceFile(),
+      gradleRunner.resolveFile("expected", "kubernetes.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+      .contains("Using resource templates from")
+      .contains("Adding revision history limit to 2")
+      .contains("validating");
+  }
+
+  @Test
+  void ocResource_withCustomMappings() throws Exception {
+    // When
+    final BuildResult result = gradleRunner.withITProject("fragments-custom-mapping")
+      .withArguments("ocResource").build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultOpenShiftResourceFile(),
+      gradleRunner.resolveFile("expected", "openshift.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+      .contains("Using resource templates from")
+      .contains("Adding revision history limit to 2")
+      .contains("validating");
+  }
+}
