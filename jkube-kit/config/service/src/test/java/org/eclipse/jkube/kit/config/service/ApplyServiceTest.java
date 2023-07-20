5da6068d7756784aef9569568ee80f98da97e296
Marc Nuri <marc@marcnuri.com>
2023-07-20 11:21:41 +0200

    chore: update license headers to comply with updated license maven plugin config
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- jkube-kit/config/service/src/test/java/org/eclipse/jkube/kit/config/service/ApplyServiceTest.java
+++ jkube-kit/config/service/src/test/java/org/eclipse/jkube/kit/config/service/ApplyServiceTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) 2019 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -11,7 +11,6 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-
 package org.eclipse.jkube.kit.config.service;
 
 import java.io.File;
