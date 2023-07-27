0e9990b8c4e5ce771ef116409f0cb6e6e45dcd53
Rohan Kumar <rohaan@redhat.com>
2023-07-27 16:17:23 +0530

    test (gradle-plugin/it) : Add tests to verify http port is picked from application.properties for health checks
    + Add test case in QuarkusHealthCheckEnricherTest to verify port
      configured in application.properties gets reflected in generated
      health checks.
    + Add gradle integration test to verify configured quarkus.http.port is
      picked up during k8sResource and is reflected in generated Kubernetes
      manifests.
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/QuarkusPropertiesIT.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.tests;
+
+import net.minidev.json.parser.ParseException;
+import org.eclipse.jkube.kit.common.ResourceVerify;
+import org.gradle.testkit.runner.BuildResult;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class QuarkusPropertiesIT {
+  @RegisterExtension
+  private final ITGradleRunnerExtension gradleRunner = new ITGradleRunnerExtension();
+
+  @Test
+  void k8sResource_whenRun_generatesK8sManifestsWithProbes() throws IOException, ParseException {
+    // When
+    final BuildResult result = gradleRunner.withITProject("quarkus-properties")
+        .withArguments("build", "k8sResource", "--stacktrace")
+        .build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultKubernetesResourceFile(),
+        gradleRunner.resolveFile("expected", "kubernetes.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("jkube-controller: Adding a default Deployment")
+        .contains("jkube-service: Adding a default service")
+        .contains("jkube-healthcheck-quarkus: Adding readiness probe on port 8008")
+        .contains("jkube-healthcheck-quarkus: Adding liveness probe on port 8008")
+        .contains("jkube-service-discovery: Using first mentioned service port '8008' ")
+        .contains("jkube-revision-history: Adding revision history limit to 2");
+  }
+
+  @Test
+  void ocResource_whenRun_generatesOpenShiftManifestsWithProbes() throws IOException, ParseException {
+    // When
+    final BuildResult result = gradleRunner.withITProject("quarkus-properties")
+        .withArguments("build", "ocResource", "--stacktrace")
+        .build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultOpenShiftResourceFile(),
+        gradleRunner.resolveFile("expected", "openshift.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("jkube-controller: Adding a default Deployment")
+        .contains("jkube-service: Adding a default service")
+        .contains("jkube-openshift-deploymentconfig: Converting Deployment to DeploymentConfig")
+        .contains("jkube-healthcheck-quarkus: Adding readiness probe on port 8008")
+        .contains("jkube-healthcheck-quarkus: Adding liveness probe on port 8008")
+        .contains("jkube-service-discovery: Using first mentioned service port '8008' ")
+        .contains("jkube-revision-history: Adding revision history limit to 2");
+  }
+}
