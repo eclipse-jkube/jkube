c30cc64a4737304cb8c7b02bb493cde066b6e648
Sun Seng David TAN <sutan@redhat.com>
2023-09-01 16:52:25 +0200

    fixup: adding missing license header
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

--- scripts/jrelease.java
+++ scripts/jrelease.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 // //DEPS <dependency1> <dependency2>
 //DEPS org.jline:jline:3.16.0
