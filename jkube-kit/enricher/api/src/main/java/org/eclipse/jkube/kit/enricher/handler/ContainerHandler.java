4c01a6dc4b3aef2f9d91f3284e26bf4c2862e005
zangus <tw_zangus@hotmail.com>
2024-01-27 00:00:21 +0100

    refactor: moved PortMapping from package org.eclipse.jkube.kit.build.api.model to org.eclipse.jkube.kit.common
    Signed-off-by: zangus <tw_zangus@hotmail.com>

--- jkube-kit/enricher/api/src/main/java/org/eclipse/jkube/kit/enricher/handler/ContainerHandler.java
+++ jkube-kit/enricher/api/src/main/java/org/eclipse/jkube/kit/enricher/handler/ContainerHandler.java
@@ -28,7 +28,7 @@ import io.fabric8.kubernetes.api.model.SecurityContext;
 import io.fabric8.kubernetes.api.model.SecurityContextBuilder;
 import io.fabric8.kubernetes.api.model.VolumeMount;
 import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
-import org.eclipse.jkube.kit.build.api.model.PortMapping;
+import org.eclipse.jkube.kit.common.PortMapping;
 import org.eclipse.jkube.kit.config.image.ImageConfiguration;
 import org.eclipse.jkube.kit.common.util.EnvUtil;
 import org.eclipse.jkube.kit.common.util.KubernetesHelper;
