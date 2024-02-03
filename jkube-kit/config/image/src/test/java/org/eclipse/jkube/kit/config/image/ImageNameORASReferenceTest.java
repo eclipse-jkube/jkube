e82a3abe7cca983d495d9a1bb4e67d850b08f325
Devashishbasu <devashishbasu587@gmail.com>
2024-02-03 19:53:54 +0530

    Update ImageNameORASReferenceTest.java

--- jkube-kit/config/image/src/test/java/org/eclipse/jkube/kit/config/image/ImageNameORASReferenceTest.java
+++ jkube-kit/config/image/src/test/java/org/eclipse/jkube/kit/config/image/ImageNameORASReferenceTest.java
@@ -60,7 +60,7 @@ class ImageNameORASReferenceTest {
       "registry.example.com/hello-world:foobar:sha256:b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9", // Invalid Digest prefix: colon instead of the at sign
       "registry.example.com/hello-world@@sha256:b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9", // Invalid Digest prefix: double at sign
       "registry.example.com/hello-world @sha256:b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9", // Invalid Digest prefix: space
-      //"registry.example.com/foobar@sha256:b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde" 
+      //"registry.example.com/foobar@sha256:b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde" // https://github.com/eclipse/jkube/issues/2543
   })
   void invalidImageNames(String name) {
     assertThatIllegalArgumentException().isThrownBy(() -> new ImageName(name));
