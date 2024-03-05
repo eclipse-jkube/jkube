c1e60d8dde73b8fca8c6b567c774e4901cc0478f
Jurrie Overgoor <1213142+Jurrie@users.noreply.github.com>
2024-03-05 06:16:35 +0100

    refactor: introduce AbstractHelmMojo class (#2714)
    * Introduce AbstractHelmMojo class
    
    When new Helm goals will be written, this class will serve as a base
    class. Previously, the HelmMojo class served as both a Helm goal and a
    base class for other Helm goals (like helm-push and helm-lint).
    
    
    * Implement pull request comments
    
    
    * Add a quick note on the separate integration tests repository

--- kubernetes-maven-plugin/plugin/src/main/java/org/eclipse/jkube/maven/plugin/mojo/build/HelmMojo.java
+++ kubernetes-maven-plugin/plugin/src/main/java/org/eclipse/jkube/maven/plugin/mojo/build/HelmMojo.java
@@ -15,81 +15,64 @@ package org.eclipse.jkube.maven.plugin.mojo.build;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
-
-import org.eclipse.jkube.kit.resource.helm.HelmConfig;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProjectHelper;
-
-import static org.eclipse.jkube.kit.resource.helm.HelmServiceUtil.initHelmConfig;
+import org.eclipse.jkube.kit.resource.helm.GeneratedChartListener;
+import org.eclipse.jkube.kit.resource.helm.HelmConfig;
 
 /**
- * Generates a Helm chart for the kubernetes resources
+ * Generates a Helm chart for the Kubernetes resources
  */
 @Mojo(name = "helm", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
-public class HelmMojo extends AbstractJKubeMojo {
+public class HelmMojo extends AbstractHelmMojo {
+
+  /**
+   * The generated Kubernetes YAML file
+   */
+  @Parameter(property = "jkube.kubernetesManifest", defaultValue = "${basedir}/target/classes/META-INF/jkube/kubernetes.yml")
+  private File kubernetesManifest;
 
   @Component
   MavenProjectHelper projectHelper;
 
-  /**
-   * The generated kubernetes YAML file
-   */
-  @Parameter(property = "jkube.kubernetesManifest", defaultValue = "${basedir}/target/classes/META-INF/jkube/kubernetes.yml")
-  File kubernetesManifest;
+  @Override
+  public void init() throws MojoFailureException {
+    super.init();
 
-  /**
-   * One of:
-   * <ul>
-   *  <li>A directory containing OpenShift Templates to use as Helm parameters.</li>
-   *  <li>A file containing a Kubernetes List with OpenShift Template entries to be used as Helm parameters.</li>
-   * </ul>
-   */
-  @Parameter(property = "jkube.kubernetesTemplate", defaultValue = "${basedir}/target/classes/META-INF/jkube/kubernetes")
-  File kubernetesTemplate;
+    final File manifest = getKubernetesManifest();
+    if (manifest == null || !manifest.isFile()) {
+      logManifestNotFoundWarning(manifest);
+    }
 
-  @Parameter
-  HelmConfig helm;
+    final GeneratedChartListener generatedChartListener = (helmConfig, type, chartFile) -> projectHelper.attachArtifact(project, helmConfig.getChartExtension(), type.getClassifier(), chartFile);
+    getHelm().getGeneratedChartListeners().add(generatedChartListener);
+  }
 
   @Override
   public void executeInternal() throws MojoExecutionException {
     try {
-      final File manifest = getKubernetesManifest();
-      if (manifest == null || !manifest.isFile()) {
-        logManifestNotFoundWarning(manifest);
-      }
-      helm = initHelmConfig(getDefaultHelmType(), javaProject, getKubernetesTemplate(), helm)
-          .generatedChartListeners(Collections.singletonList((helmConfig, type, chartFile) -> projectHelper
-              .attachArtifact(project, helmConfig.getChartExtension(), type.getClassifier(), chartFile)))
-          .build();
-      jkubeServiceHub.getHelmService().generateHelmCharts(helm);
+      jkubeServiceHub.getHelmService().generateHelmCharts(getHelm());
     } catch (IOException exception) {
       throw new MojoExecutionException(exception.getMessage());
     }
   }
 
   protected void logManifestNotFoundWarning(File manifest) {
-    getKitLogger().warn("No kubernetes manifest file has been generated yet by the k8s:resource goal at: " + manifest);
+    getKitLogger().warn("No Kubernetes manifest file has been generated yet by the k8s:resource goal at: " + manifest);
   }
 
   protected File getKubernetesManifest() {
     return kubernetesManifest;
   }
 
-  protected File getKubernetesTemplate() {
-     return kubernetesTemplate;
-  }
-
+  @Override
   protected HelmConfig.HelmType getDefaultHelmType() {
     return HelmConfig.HelmType.KUBERNETES;
-  }
-
-  HelmConfig getHelm() {
-    return helm;
   }
 }
