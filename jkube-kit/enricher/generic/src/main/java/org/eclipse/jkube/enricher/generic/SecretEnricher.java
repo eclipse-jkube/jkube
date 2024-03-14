329abc9d9c4e04a8b708a7f3bf3f615e30c6b754
Oleksandr Krutko <46520164+arsenalzp@users.noreply.github.com>
2024-03-14 08:36:03 +0100

    Remove unused import in SecretEnricher class

--- jkube-kit/enricher/generic/src/main/java/org/eclipse/jkube/enricher/generic/SecretEnricher.java
+++ jkube-kit/enricher/generic/src/main/java/org/eclipse/jkube/enricher/generic/SecretEnricher.java
@@ -33,7 +33,6 @@ import org.eclipse.jkube.kit.enricher.api.model.Configuration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
