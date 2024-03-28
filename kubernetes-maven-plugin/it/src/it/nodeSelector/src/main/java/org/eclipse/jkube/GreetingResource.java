37498f7374b9594eb7b98f85a11dae43374deb1c
Sun Seng David TAN <sutan@redhat.com>
2024-03-28 11:19:51 +0100

    test(controller-enricher): Add Maven integration test for nodeSelector via Maven XML and Groovy DSL
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

---
+++ kubernetes-maven-plugin/it/src/it/nodeSelector/src/main/java/org/eclipse/jkube/GreetingResource.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello JKube";
+    }
+}
