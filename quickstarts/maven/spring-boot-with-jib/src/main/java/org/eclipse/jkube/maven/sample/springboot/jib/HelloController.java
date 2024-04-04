28f1b5171d764e14c59aa742198cf48d3477d699
waynemorphic <wayneklyde@gmail.com>
2024-04-04 17:10:24 +0300

    Switched to GetMapping annotation Signed-off-by:wayne kirimi <wayneklyde@gmail.com>

--- quickstarts/maven/spring-boot-with-jib/src/main/java/org/eclipse/jkube/maven/sample/springboot/jib/HelloController.java
+++ quickstarts/maven/spring-boot-with-jib/src/main/java/org/eclipse/jkube/maven/sample/springboot/jib/HelloController.java
@@ -13,14 +13,14 @@
  */
 package org.eclipse.jkube.maven.sample.springboot.jib;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot(Powered by JIB)!!";
     }
