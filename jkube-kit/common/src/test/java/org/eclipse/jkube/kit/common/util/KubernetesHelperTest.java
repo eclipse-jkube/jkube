932966e9a50e87b5588195770224b3dd30191b95
Sintivrousai <116221123+Sintivrousai@users.noreply.github.com>
2024-04-22 07:30:04 +0300

    fix: remove unused imports from KubernetesHelperTest #2926 (#2949)

--- jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/KubernetesHelperTest.java
+++ jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/KubernetesHelperTest.java
@@ -20,7 +20,6 @@ import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
