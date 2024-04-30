bb50a22d839da320e2b72566e104f05c9c460c15
Marc Nuri <marc@marcnuri.com>
2024-04-30 13:04:06 +0200

    review: build args helper
    Simplify logic to allow for easier future developments and improvements.
    
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- jkube-kit/build/api/src/main/java/org/eclipse/jkube/kit/build/api/helper/BuildArgResolverUtil.java
+++ jkube-kit/build/api/src/main/java/org/eclipse/jkube/kit/build/api/helper/BuildArgResolverUtil.java
@@ -15,23 +15,24 @@ package org.eclipse.jkube.kit.build.api.helper;
 
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jkube.kit.common.JKubeConfiguration;
+import org.eclipse.jkube.kit.common.JKubeException;
 import org.eclipse.jkube.kit.config.image.ImageConfiguration;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.Properties;
-
-import static org.eclipse.jkube.kit.common.util.MapUtil.mergeMapsImmutable;
+import java.util.stream.Stream;
 
 public class BuildArgResolverUtil {
+
   private static final String ARG_PREFIX = "docker.buildArg.";
 
   private BuildArgResolverUtil() { }
 
   /**
-   * Merges Docker Build Args in the following order (in decreasing order of precedence):
+   * Merges Docker Build Args from the following sources:
    * <ul>
    *   <li>Build Args specified directly in ImageConfiguration</li>
    *   <li>Build Args specified via System Properties</li>
@@ -39,23 +40,32 @@ public class BuildArgResolverUtil {
    *   <li>Build Args specified via Plugin configuration</li>
    *   <li>Docker Proxy Build Args detected from ~/.docker/config.json</li>
    * </ul>
-   * @param imageConfig ImageConfiguration for which Build Args would be resolved
-   * @param configuration {@link JKubeConfiguration} JKubeConfiguration
+   * @param imageConfig ImageConfiguration where to get the Build Args from.
+   * @param configuration {@link JKubeConfiguration}.
    * @return a Map containing merged Build Args from all sources.
    */
   public static Map<String, String> mergeBuildArgs(ImageConfiguration imageConfig, JKubeConfiguration configuration) {
-    Map<String, String> buildArgsFromProjectProperties = addBuildArgsFromProperties(configuration.getProject().getProperties());
-    Map<String, String> buildArgsFromSystemProperties = addBuildArgsFromProperties(System.getProperties());
-    Map<String, String> buildArgsFromDockerConfig = addBuildArgsFromDockerConfig();
-
-    return mergeMapsImmutable(imageConfig.getBuild().getArgs(),
-        buildArgsFromSystemProperties,
-        buildArgsFromProjectProperties,
-        Optional.ofNullable(configuration.getBuildArgs()).orElse(Collections.emptyMap()),
-        buildArgsFromDockerConfig);
+    final Map<String, String> buildArgs = new HashMap<>();
+    Stream.of(
+      imageConfig.getBuild().getArgs(),
+      buildArgsFromProperties(System.getProperties()),
+      buildArgsFromProperties(configuration.getProject().getProperties()),
+      configuration.getBuildArgs(),
+      buildArgsFromDockerConfig()
+    )
+      .filter(Objects::nonNull)
+      .flatMap(map -> map.entrySet().stream())
+      .forEach(entry -> {
+        if (buildArgs.containsKey(entry.getKey())) {
+          throw new JKubeException(String.format("Multiple Build Args with the same key: %s=%s and %s=%s",
+            entry.getKey(), buildArgs.get(entry.getKey()), entry.getKey(), entry.getValue()));
+        }
+        buildArgs.put(entry.getKey(), entry.getValue());
+      });
+    return buildArgs;
   }
 
-  private static Map<String, String> addBuildArgsFromProperties(Properties properties) {
+  private static Map<String, String> buildArgsFromProperties(Properties properties) {
     Map<String, String> buildArgs = new HashMap<>();
     for (Object keyObj : properties.keySet()) {
       String key = (String) keyObj;
@@ -71,7 +81,7 @@ public class BuildArgResolverUtil {
     return buildArgs;
   }
 
-  private static Map<String, String> addBuildArgsFromDockerConfig() {
+  private static Map<String, String> buildArgsFromDockerConfig() {
     final Map<String, Object> dockerConfig = DockerFileUtil.readDockerConfig();
     if (dockerConfig == null) {
       return Collections.emptyMap();
