ede0d78106b5a661e0f1a43b4074a2569bd14337
Jason Mok <106209849+jasonmokk@users.noreply.github.com>
2024-05-09 14:26:46 -0500

    Revert

--- jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/SemanticVersionUtilTest.java
+++ jkube-kit/common/src/test/java/org/eclipse/jkube/kit/common/util/SemanticVersionUtilTest.java
@@ -13,13 +13,13 @@
  */
 package org.eclipse.jkube.kit.common.util;
 
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.eclipse.jkube.kit.common.util.SemanticVersionUtil.isVersionAtLeast;
