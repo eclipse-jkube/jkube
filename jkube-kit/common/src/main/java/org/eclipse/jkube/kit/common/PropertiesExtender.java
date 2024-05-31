a56d7fa4daf2cb8733162425502ede3010891e09
Oleksandr Krutko <alexander.krutko@gmail.com>
2024-05-31 21:59:57 +0300

    fix typos, fix DefaultGeneratorManager test, add licence license header
    Signed-off-by: Oleksandr Krutko <alexander.krutko@gmail.com>

--- jkube-kit/common/src/main/java/org/eclipse/jkube/kit/common/PropertiesExtender.java
+++ jkube-kit/common/src/main/java/org/eclipse/jkube/kit/common/PropertiesExtender.java
@@ -1,16 +1,27 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
 package org.eclipse.jkube.kit.common;
 
 import java.net.URL;
 import java.util.Properties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class PropertiesExtender extends Properties {
+  @Getter
+  @Setter
   private URL propertiesFile;
-  
-  public void setPropertiesFile(URL file) {
-    this.propertiesFile = file;
-  }
-  
-  public URL getPropertiesFile() {
-    return this.propertiesFile;
-  }
 }
