ad717ae37a80702201575a14e0eb5e634175c6e1
Luigi De Masi <5583341+luigidemasi@users.noreply.github.com>
2024-08-12 12:35:51 +0200

    OpenShift route generator: prefer port name over port number for targetPort when it's possible

--- jkube-kit/enricher/generic/src/main/java/org/eclipse/jkube/enricher/generic/openshift/RouteEnricher.java
+++ jkube-kit/enricher/generic/src/main/java/org/eclipse/jkube/enricher/generic/openshift/RouteEnricher.java
@@ -14,6 +14,7 @@
 package org.eclipse.jkube.enricher.generic.openshift;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.ServicePort;
 import io.fabric8.openshift.api.model.RouteSpec;
 import org.eclipse.jkube.kit.common.Configs;
 import org.eclipse.jkube.kit.common.util.FileUtil;
@@ -37,8 +38,7 @@ import org.eclipse.jkube.kit.enricher.api.ServiceExposer;
 
 import java.util.Objects;
 
-import static org.eclipse.jkube.enricher.generic.DefaultServiceEnricher.getPortToExpose;
-import static org.eclipse.jkube.enricher.generic.DefaultServiceEnricher.getTargetPortToExpose;
+import static org.eclipse.jkube.enricher.generic.DefaultServiceEnricher.getServicePortToExpose;
 import static org.eclipse.jkube.kit.enricher.api.util.KubernetesResourceUtil.mergeMetadata;
 import static org.eclipse.jkube.kit.enricher.api.util.KubernetesResourceUtil.mergeSimpleFields;
 import static org.eclipse.jkube.kit.enricher.api.util.KubernetesResourceUtil.removeItemFromKubernetesBuilder;
@@ -121,14 +121,23 @@ public class RouteEnricher extends BaseEnricher implements ServiceExposer {
         }
     }
 
-    private static RoutePort createRoutePort(ServiceBuilder serviceBuilder) {
-        RoutePort routePort = null;
-        final Integer serviceTargetPort = getTargetPortToExpose(serviceBuilder);
-        final Integer servicePort = serviceTargetPort != null ? serviceTargetPort : getPortToExpose(serviceBuilder);
-        if (servicePort != null) {
-            routePort = new RoutePort();
-            routePort.setTargetPort(new IntOrString(servicePort));
+    private static RoutePort createRoutePort(ServicePort servicePortToExpose) {
+        RoutePort routePort = new RoutePort();
+        final String servicePortName = servicePortToExpose.getName();
+
+        if(servicePortName != null){
+            routePort.setTargetPort(new IntOrString(servicePortName));
+            return routePort;
         }
+
+        final IntOrString serviceTargetPort = servicePortToExpose.getTargetPort();
+        if( serviceTargetPort != null && serviceTargetPort.getValue() != null){
+            routePort.setTargetPort(serviceTargetPort);
+            return routePort;
+        }
+
+        final Integer servicePort =  servicePortToExpose.getPort();
+        routePort.setTargetPort(new IntOrString(servicePort));
         return routePort;
     }
 
@@ -224,8 +233,17 @@ public class RouteEnricher extends BaseEnricher implements ServiceExposer {
     static Route createOpinionatedRouteFromService(ServiceBuilder serviceBuilder, String routeDomainPostfix, String tlsTermination, String edgeTerminationPolicy, boolean isRouteWithTls) {
         ObjectMeta serviceMetadata = serviceBuilder.buildMetadata();
         if (serviceMetadata != null) {
+            RoutePort routePort = null;
             String name = serviceMetadata.getName();
-            RoutePort routePort = createRoutePort(serviceBuilder);
+            final ServicePort servicePortToExpose = getServicePortToExpose(serviceBuilder);
+
+            if(servicePortToExpose != null && (
+                            servicePortToExpose.getName() != null ||
+                            servicePortToExpose.getPort() != null ||
+                            servicePortToExpose.getTargetPort() != null)){
+                routePort = createRoutePort(servicePortToExpose);
+            }
+
             if (routePort != null) {
                 RouteBuilder routeBuilder = new RouteBuilder().
                         withMetadata(serviceMetadata).
