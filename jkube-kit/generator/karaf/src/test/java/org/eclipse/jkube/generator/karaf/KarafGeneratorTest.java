d83b1fe2ea0587b72a0f19b5430426310aa1970d
Shivam <shivamrc189@gmail.com>
2024-10-10 11:20:53 +0530

    Replaced asList() with The asInstanceOf(InstanceOfAssertFactories.list(Type.class)) AsertJ's method

--- jkube-kit/generator/karaf/src/test/java/org/eclipse/jkube/generator/karaf/KarafGeneratorTest.java
+++ jkube-kit/generator/karaf/src/test/java/org/eclipse/jkube/generator/karaf/KarafGeneratorTest.java
@@ -22,11 +22,8 @@ import java.util.Properties;
 
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.eclipse.jkube.generator.api.GeneratorContext;
-import org.eclipse.jkube.kit.common.JavaProject;
-import org.eclipse.jkube.kit.common.KitLogger;
+import org.eclipse.jkube.kit.common.*;
 import org.eclipse.jkube.kit.config.image.ImageConfiguration;
-import org.eclipse.jkube.kit.common.Plugin;
-import org.eclipse.jkube.kit.common.AssemblyConfiguration;
 import org.eclipse.jkube.kit.config.image.build.BuildConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -104,7 +101,8 @@ class KarafGeneratorTest {
     assertThat(result.iterator().next().getBuildConfiguration().getAssembly())
         .hasFieldOrPropertyWithValue("name", "deployments")
         .hasFieldOrPropertyWithValue("excludeFinalOutputArtifact", false)
-        .extracting(AssemblyConfiguration::getLayers).asList()
+        .extracting(AssemblyConfiguration::getLayers)
+        .asInstanceOf(InstanceOfAssertFactories.list(Assembly.class))
         .singleElement()
         .extracting("fileSets").asList()
         .extracting("directory", "outputDirectory", "directoryMode", "fileMode")
