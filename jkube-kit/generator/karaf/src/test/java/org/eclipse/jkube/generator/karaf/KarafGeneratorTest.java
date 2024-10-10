ea007aa4e25b98efd7e8d1ae98016d7eca4000fe
Shivam <96533413+ShivamChavan01@users.noreply.github.com>
2024-10-10 13:40:41 +0530

    fix: replace AssertJ's deprecated asList() DSL method in KarafGeneratorTest (#3446)
    Replaced asList() with The asInstanceOf(InstanceOfAssertFactories.list(Type.class)) AsertJ's method
    ---
    Removed Unused Imports

--- jkube-kit/generator/karaf/src/test/java/org/eclipse/jkube/generator/karaf/KarafGeneratorTest.java
+++ jkube-kit/generator/karaf/src/test/java/org/eclipse/jkube/generator/karaf/KarafGeneratorTest.java
@@ -26,6 +26,7 @@ import org.eclipse.jkube.kit.common.JavaProject;
 import org.eclipse.jkube.kit.common.KitLogger;
 import org.eclipse.jkube.kit.config.image.ImageConfiguration;
 import org.eclipse.jkube.kit.common.Plugin;
+import org.eclipse.jkube.kit.common.Assembly;
 import org.eclipse.jkube.kit.common.AssemblyConfiguration;
 import org.eclipse.jkube.kit.config.image.build.BuildConfiguration;
 import org.junit.jupiter.api.BeforeEach;
@@ -104,7 +105,8 @@ class KarafGeneratorTest {
     assertThat(result.iterator().next().getBuildConfiguration().getAssembly())
         .hasFieldOrPropertyWithValue("name", "deployments")
         .hasFieldOrPropertyWithValue("excludeFinalOutputArtifact", false)
-        .extracting(AssemblyConfiguration::getLayers).asList()
+        .extracting(AssemblyConfiguration::getLayers)
+        .asInstanceOf(InstanceOfAssertFactories.list(Assembly.class))
         .singleElement()
         .extracting("fileSets").asList()
         .extracting("directory", "outputDirectory", "directoryMode", "fileMode")
