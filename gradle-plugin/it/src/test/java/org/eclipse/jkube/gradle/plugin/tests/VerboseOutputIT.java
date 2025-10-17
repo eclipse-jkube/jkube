cba05046e0c4e50ad9db193208ac91d3586cfc26
Ashish Thakur <ashishth@redhat.com>
2025-10-17 16:36:50 +0530

    add IT test for the verbose props

---
+++ gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/VerboseOutputIT.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.gradle.plugin.tests;
+
+import net.minidev.json.parser.ParseException;
+import org.eclipse.jkube.kit.common.ResourceVerify;
+import org.gradle.testkit.runner.BuildResult;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class VerboseOutputIT {
+
+  @RegisterExtension
+  private final ITGradleRunnerExtension gradleRunner = new ITGradleRunnerExtension();
+
+  @Test
+  void k8sResource_whenVerboseEnabled_generatesK8sManifestsWithVerboseOutput() throws IOException, ParseException {
+    // When
+    final BuildResult result = gradleRunner.withITProject("verbose-output").withArguments("k8sResource").build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultKubernetesResourceFile(),
+        gradleRunner.resolveFile("expected", "kubernetes.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("Generators:")
+        .contains("Enrichers:");
+  }
+
+  @Test
+  void ocResource_whenVerboseEnabled_generatesOpenShiftManifestsWithVerboseOutput() throws IOException, ParseException {
+    // When
+    final BuildResult result = gradleRunner.withITProject("verbose-output").withArguments("ocResource").build();
+    // Then
+    ResourceVerify.verifyResourceDescriptors(gradleRunner.resolveDefaultOpenShiftResourceFile(),
+        gradleRunner.resolveFile("expected", "openshift.yml"));
+    assertThat(result).extracting(BuildResult::getOutput).asString()
+        .contains("Generators:")
+        .contains("Enrichers:");
+  }
+}
