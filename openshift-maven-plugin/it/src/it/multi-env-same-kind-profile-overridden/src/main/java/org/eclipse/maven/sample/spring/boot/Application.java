8aa601389f0585d139d4f743305c6730c3aab2cb
Ashish Thakur <ashishth@redhat.com>
2025-11-24 13:31:33 +0530

    fix - Add IT tests

--- openshift-maven-plugin/it/src/it/multi-env-same-kind-profile-overridden/src/main/java/org/eclipse/maven/sample/spring/boot/Application.java
+++ openshift-maven-plugin/it/src/it/multi-env-same-kind-profile-overridden/src/main/java/org/eclipse/maven/sample/spring/boot/Application.java
@@ -11,7 +11,7 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.jkube.maven.sample.spring.boot;
+package org.eclipse.maven.sample.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
