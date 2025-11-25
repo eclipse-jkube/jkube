3f698271572ea5a7bdcf56ef9c795a0d9a491cce
Ashish Thakur <ashishth@redhat.com>
2025-11-25 15:19:04 +0530

    fix - fxi sonar lint issues

--- gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/MultiProfileIT.java
+++ gradle-plugin/it/src/test/java/org/eclipse/jkube/gradle/plugin/tests/MultiProfileIT.java
@@ -18,18 +18,17 @@ import java.io.IOException;
 import org.eclipse.jkube.kit.common.ResourceVerify;
 
 import net.minidev.json.parser.ParseException;
-import org.gradle.testkit.runner.BuildResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-public class MultiProfileIT {
+class MultiProfileIT {
   @RegisterExtension
   private final ITGradleRunnerExtension gradleRunner = new ITGradleRunnerExtension();
 
   @Test
   void ocResource_whenRunMutliFragments_generatesManifests() throws IOException, ParseException {
     // When
-    final BuildResult result = gradleRunner.withITProject("multi-env-same-kind")
+    gradleRunner.withITProject("multi-env-same-kind")
         .withArguments("build", "ocResource")
         .build();
     // Then
@@ -40,7 +39,7 @@ public class MultiProfileIT {
   @Test
   void ocResource_whenRunMutliFragmentsProfileOverridden_generatesManifests() throws IOException, ParseException {
     // When
-    final BuildResult result = gradleRunner.withITProject("multi-env-same-kind-profile-overridden")
+    gradleRunner.withITProject("multi-env-same-kind-profile-overridden")
         .withArguments("build", "ocResource")
         .build();
     // Then
